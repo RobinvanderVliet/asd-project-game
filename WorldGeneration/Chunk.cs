19fed5964b4caa58ac38c6ec37a78893e0f54dc1
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-04-16 17:35:53 +0200

    database functies organisatie en gitignore added

--- WorldGeneration/Chunk.cs
+++ WorldGeneration/Chunk.cs
@@ -2,6 +2,13 @@ namespace WorldGeneration
 {
     public class Chunk
     {
+        public Chunk(int x, int y, Tile[,] map)
+        {
+            this.x = x;
+            this.y = y;
+            this.map = map;
+        }
+
         public int x { get; set; }
         public int y { get; set; }
         public Tile[,] map { get; set; }
