e4ac5af73c7598003fb32b62771ddad396443c47
Sven van Beek <SHT.vanBeek@student.han.nl>
2021-04-16 14:17:47 +0200

    Intial commit unit test

---
+++ Chat.Tests/ParserTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Chat.antlr.ast;
+using NUnit.Framework;
+
+namespace Chat.Tests
+{
+    public class ParserTest
+    {
+        [Test]
+        public void EnterMovePushToCurrentContainerTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
