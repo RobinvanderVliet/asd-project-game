cd64a4dd8a32cc854fa215db9959c61b030abfec
Pepijn van Erp <ppb.vanerp@student.han.nl>
2021-04-16 15:42:52 +0200

    created Receiver class

---
+++ Network/Receiver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    class Receiver {
+
+        //Receive message from socket
+
+
+    }
+}
