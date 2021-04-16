ace0b99ad9597598e372fb3ad7d49aa19804e9a4
Martijn <martijn.lankhof@gmail.com>
2021-04-16 13:22:08 +0200

    Added first nodes to agent AST

---
+++ Agent.Tests/ConfigurationTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Agent.Tests
+{
+    [TestFixture]
+    public class ConfigurationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,1);
+        }
+        
+    }
+}
