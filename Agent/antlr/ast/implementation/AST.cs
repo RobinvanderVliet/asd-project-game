91b46e7a81d63731378879beefe8367a48249db6
Martijn <martijn.lankhof@gmail.com>
2021-04-19 13:14:05 +0200

    added AST to agent AST tree

--- Agent/antlr/ast/implementation/AST.cs
+++ Agent/antlr/ast/implementation/AST.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Agent.antlr.ast.interfaces;
 
 namespace Agent.antlr.ast.implementation
 {
+    /*
+    AIM SD ASD 2020/2021 S2 project
+     
+    Project name: [to be determined].
+
+    This file is created by team: 1.
+     
+    Goal of this file: [making_the_system_work].
+     
+    */
     class AST
     {
+        public IConfiguration root;
+
+        public AST( )
+        {
+            this.root = new Configuration();
+        }
+        public AST(IConfiguration root)
+        {
+            this.root = root;
+        }
     }
 }
