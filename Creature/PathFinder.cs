1a1eb5ab7d6eaae4763c72f3af8294681cf17979
JW.vanRijswijk <jw.vanrijswijk@student.han.nl>
2021-04-19 10:13:27 +0200

    Fixed order of classes in pathfinding algorithm

--- Creature/PathFinder.cs
+++ Creature/PathFinder.cs
@@ -7,6 +7,42 @@ using System.Threading.Tasks;
 
 namespace Creature
 {
+    class Node
+    {
+        public const int nodeSize = 1;
+        public Node parent;
+        public Vector2 position;
+        public Vector2 center
+        {
+            get
+            {
+                return new Vector2(position.X + nodeSize / 2, position.Y + nodeSize / 2);
+            }
+        }
+        public float distanceToTarget;
+        public float cost;
+        public float weight;
+        public float fScore
+        {
+            get
+            {
+                if (distanceToTarget != -1 && cost != -1)
+                    return distanceToTarget + cost;
+                else
+                    return -1;
+            }
+        }
+        public bool isWalkable;
+        public Node(Vector2 pos, bool isWalkable, float weight = 1)
+        {
+            this.parent = null;
+            this.position = pos;
+            this.distanceToTarget = -1;
+            this.cost = 1;
+            this.weight = weight;
+            this.isWalkable = isWalkable;
+        }
+    }
     class PathFinder
     {
         List<List<Node>> grid;
@@ -106,42 +142,6 @@ namespace Creature
             }
 
             return temp;
-        }
-    }
-    class Node
-    {
-        public const int nodeSize = 1;
-        public Node parent;
-        public Vector2 position;
-        public Vector2 center
-        {
-            get
-            {
-                return new Vector2(position.X + nodeSize / 2, position.Y + nodeSize / 2);
-            }
-        }
-        public float distanceToTarget;
-        public float cost;
-        public float weight;
-        public float fScore
-        {
-            get
-            {
-                if (distanceToTarget != -1 && cost != -1)
-                    return distanceToTarget + cost;
-                else
-                    return -1;
-            }
-        }
-        public bool isWalkable;
-        public Node(Vector2 pos, bool isWalkable, float weight = 1)
-        {
-            this.parent = null;
-            this.position = pos;
-            this.distanceToTarget = -1;
-            this.cost = 1;
-            this.weight = weight;
-            this.isWalkable = isWalkable;
         }
     }
 }
