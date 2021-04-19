d7fa99dc42d8f8e810ad0e069b2f7392c2e5a5b2
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-04-19 10:41:37 +0200

    Added main method for Creature prototype

---
+++ Creature/CreaturePrototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creature
+{
+    class CreaturePrototype
+    {
+        static void Main(string[] args)
+        {
+            // Prototype code here...
+        }
+    }
+}
