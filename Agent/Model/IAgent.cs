1f7838b557407c8f7b60dcd6e83e8e3c90e5e0f1
Mark Brouwer <mbbrou@gmail.com>
2021-04-19 15:43:23 +0200

    Added first version of the interface

---
+++ Agent/Model/IAgent.cs
@@ -0,0 +1,17 @@
+﻿namespace Agent.Model
+{
+    public interface IAgent
+    {
+        public void SwitchAgent();
+
+        public void LoadConfiguration(AgentConfiguration agentConfiguration);
+
+        public void ExecuteAttackAction();
+
+        public void ExecuteCollectAction();
+
+        public void ExecuteCheckAction();
+        
+        public void ExecuteCommand();
+    }
+}
