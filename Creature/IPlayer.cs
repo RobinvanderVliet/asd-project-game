7b4f51ca5f23a790e9a25b479e65ace5b087b0ac
Marco Beijen <m.beijen1@student.han.nl>
2021-04-20 16:58:17 +0200

    Added the player interface for the prototype.

--- Creature/IPlayer.cs
+++ Creature/IPlayer.cs
@@ -1,6 +1,13 @@
-﻿namespace Creature
+﻿using System.Numerics;
+
+namespace Creature
 {
     internal interface IPlayer
     {
+        bool IsAlive { get; set; }
+        Vector2 Position { get; set; }
+        public void ApplyDamage(double amount);
+
+        public void HealAmount(double amount);
     }
 }
