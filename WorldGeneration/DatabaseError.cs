004ccac5b27827716e7f628522526e29e6c1a105
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-04-20 16:15:38 +0200

    Werkend gemaakt omdat het af moet zijn. Also headers toegevoegd

--- WorldGeneration/DatabaseError.cs
+++ WorldGeneration/DatabaseError.cs
@@ -1,3 +1,15 @@
+/*
+    AIM SD ASD 2020/2021 S2 project
+     
+    Project name: ASD project.
+ 
+    This file is created by team: 3.
+     
+    Goal of this file: Special error to make it clear what has gone wrong.
+     
+*/
+
+#nullable enable
 using System;
 
 namespace WorldGeneration
--- WorldGeneration/Tile.cs
+++ WorldGeneration/Tile.cs
@@ -1,3 +1,14 @@
+/*
+    AIM SD ASD 2020/2021 S2 project
+     
+    Project name: ASD project.
+ 
+    This file is created by team: 3.
+     
+    Goal of this file: Storing tile data. Possible executing functions if needed in future.
+     
+*/
+
 namespace WorldGeneration
 {
     public class Tile
