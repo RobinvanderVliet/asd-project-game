a1944b3c70adb1e58f0dd50990a162d962fd3c87
R-Gerrits <48597297+R-Gerrits@users.noreply.github.com>
2021-04-20 12:11:44 +0200

    Update IConsumable

--- Creature/Consumable/IConsumable.cs
+++ Creature/Consumable/IConsumable.cs
@@ -8,26 +8,13 @@ namespace Creature.Consumable
 {
    public interface IConsumable
     {
-        public String type { get; set; }
-        public int amount { get; set; }
+        public String Type { get; set; }// Uses text like: "Healing for 20 points." or "Damage oppponent for 20 points.".
+        public int Amount { get; set; }
         
-        public void applyEffect(String recipient)
+        public void Use()
         {
-            //implement logic to translate the effect of the consumable to actions.
+            Console.WriteLine("Consumed consumable!");
         }
-        public void damageEnemie(String recipient)// Must be a object type from the attacking entity instead of String.
-            //implement logic to damage an player.
-        {
-        }
-        public void healPlayer(String recipient)// Must be a Object of type Player instead of String.
-        {
-            // Implement logic to heal an player.
-        }
-        public void lowerAmount()
-        {
-            this.amount -= 1;
-        }
-        
 
     }
 }
