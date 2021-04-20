d244e2c58c8a7b0c8787f2d4d88999e43592660e
marty000123 <32172578+marty000123@users.noreply.github.com>
2021-04-20 10:15:00 +0200

    added header for test class

--- Player.Tests/MovementComponentTest.cs
+++ Player.Tests/MovementComponentTest.cs
@@ -1,3 +1,14 @@
+/*
+    AIM SD ASD 2020/2021 S2 project
+     
+    Project name: ASD-Project.
+ 
+    This file is created by team: 2.
+     
+    Goal of this file: testing changing the position of the player after input.
+     
+*/
+
 using NUnit.Framework;
 
 namespace Player.Tests
