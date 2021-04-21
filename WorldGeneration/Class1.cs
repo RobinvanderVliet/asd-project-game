da19ce597241c59af2f18b88a6a1e18a4d8ad0b1
Mike <mikevh17@gmail.com>
2021-04-21 09:56:01 +0200

    Update Class1.cs
    Updated class1 because of mergeconflict

--- WorldGeneration/Class1.cs
+++ WorldGeneration/Class1.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+   AIM SD ASD 2020/2021 S2 project
+
+   Project name: ASD project.
+
+   This file is created by team: 3.
+
+   Goal of this file: Prototype database storage.
+
+*/
+
+using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
