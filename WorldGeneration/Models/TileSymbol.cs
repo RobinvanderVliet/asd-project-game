e2549421bb5d934d2000642873cace2983cfa2c3
Ugur Ekim <ugurekim@gmail.com>
2021-04-21 14:27:37 +0200

    Created TileSymbol.cs class (this wasnt added in previous commit?)

---
+++ WorldGeneration/Models/TileSymbol.cs
@@ -0,0 +1,16 @@
+namespace WorldGeneration.Models
+{
+    public static class TileSymbol
+    {
+        public const string Door = "/";
+        public const string House = "+";
+        public const string Wall = "\u25A0";
+        public const string Gas = "%";
+        public const string Spike = "^";
+        public const string Chest = "n";
+        public const string Dirt = ".";
+        public const string Grass = ",";
+        public const string Street = "_";
+        public const string Water = "~";
+    }
+}
