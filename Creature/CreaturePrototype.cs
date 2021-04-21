8657c981f9e81b6b9448e1de017f639be27527ee
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-04-21 11:11:22 +0200

    Added map with 2 creatures and 1 player that can move

--- Creature/CreaturePrototype.cs
+++ Creature/CreaturePrototype.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Creature.World;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,45 @@ namespace Creature
     {
         static void Main(string[] args)
         {
-            ICreature creature = new Monster();
-            creature.FireEvent(Monster.Event.SPOTTED_PLAYER);
+            IWorld world = new DefaultWorld(25);
+            IPlayer player = new Player(10, new Vector2(3, 2));
+            ICreature creature = new Monster(new Vector2(10, 10), 2, 6, 10);
+            ICreature creature2 = new Monster(new Vector2(20, 20), 2, 6, 10);
+
+            world.SpawnPlayer(player);
+            world.SpawnCreature(creature);
+            world.SpawnCreature(creature2);
+
+            world.Render();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                MovePlayer(player, input);
+                world.Render();
+            }
+        }
+
+        private static void MovePlayer(IPlayer player, string input)
+        {
+            switch (input)
+            {
+                case "w":
+                    player.Position = new Vector2(player.Position.X, player.Position.Y + 1);
+                    break;
+                case "a":
+                    player.Position = new Vector2(player.Position.X - 1, player.Position.Y);
+                    break;
+                case "s":
+                    player.Position = new Vector2(player.Position.X, player.Position.Y - 1);
+                    break;
+                case "d":
+                    player.Position = new Vector2(player.Position.X + 1, player.Position.Y);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
