36960bfe42f3e360c90ed710a8a9950dfd0acfb3
Sven van Beek <SHT.vanBeek@student.han.nl>
2021-04-21 09:56:06 +0200

    Add setup Evaluator.cs

---
+++ Chat/antlr/transformer/Evaluator.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using Chat.antlr.ast;
+using Chat.antlr.ast.actions;
+
+namespace Chat.antlr.transformer
+{
+    public class Evaluator : ITransform
+    {
+        public void apply(AST ast)
+        {
+            transformNode(ast.root);
+        }
+
+        private void transformNode(ASTNode node)
+        {
+            if (node is Move)
+            {
+                transformMove(node);
+            }
+        }
+
+        private void transformMove(ASTNode node)
+        {
+            Move move = (Move)node;
+            // if (move.direction.ToString() == "forward")
+            {
+                
+                // .Net methode (move.direction.ToString(), move.steps)
+            }
+        }
+    }
+}
