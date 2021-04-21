ef987b05b3b40b607a7cba145220044d725965cd
Pepijn van Erp <ppb.vanerp@student.han.nl>
2021-04-21 14:08:05 +0200

    Removed unnecessary DTO

--- Network/SessionUpdateActionDTO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-
-namespace Network
-{
-    public class SessionUpdateActionDTO
-    {
-        public ArrayList prioritizedHostList { get; set; } //UUID's of clients
-    }
-}
