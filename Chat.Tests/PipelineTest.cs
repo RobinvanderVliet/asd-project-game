1e56a99204caab60fc3ca556e24928219c4dd09a
Sven van Beek <SHT.vanBeek@student.han.nl>
2021-04-21 13:45:27 +0200

    add exception, add tests for Pipeline.cs

--- Chat.Tests/PipelineTest.cs
+++ Chat.Tests/PipelineTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Data;
 using Chat.antlr;
+using Chat.antlr.ast;
+using Chat.antlr.ast.actions;
+using Chat.exception;
 using NUnit.Framework;
 
 namespace Chat.Tests
@@ -8,8 +10,33 @@ namespace Chat.Tests
     public class PipelineTest
     {
         [Test]
-        public void Test1()
+        public void ThrowsSyntaxErrorWhenCommandNotRecognised()
         {
+            Pipeline sut = new Pipeline();
+            Assert.Throws<CommandSyntaxException>(() => sut.ParseCommand("me forward"));
+        }
+        
+        [Test]
+        public void change()
+        {
+            Pipeline sut = new Pipeline();
+            sut.ParseCommand("move forward");
+            AST ast = sut.Ast;
+            AST exp = MoveCommand(1, "forward");
+
+            Assert.AreEqual(exp, ast);
+        }
+        
+        public static AST MoveCommand(int steps, String direction)
+        {
+            Input moveForward = new Input();
+
+
+            moveForward.addChild(new Move()
+                .addChild(new Direction(direction))
+                .addChild(new Step(steps)));
+
+            return new AST(moveForward);
         }
     }
 }
