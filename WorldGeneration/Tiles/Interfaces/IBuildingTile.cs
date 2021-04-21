3ea6369a5c29c0143156e70c629d710a1305911a
Mike <mikevh17@gmail.com>
2021-04-21 11:13:53 +0200

    Removed unused usings
    Removed unused usings

--- WorldGeneration/Tiles/Interfaces/IBuildingTile.cs
+++ WorldGeneration/Tiles/Interfaces/IBuildingTile.cs
@@ -9,12 +9,6 @@
      
 */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WorldGeneration.Tiles.Interfaces
 {
     interface IBuildingTile
