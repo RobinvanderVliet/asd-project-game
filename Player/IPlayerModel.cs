0ce0e67714f14d318f00ac9f00bf44054ef05e3a
marty000123 <32172578+marty000123@users.noreply.github.com>
2021-04-21 12:06:37 +0200

    header interface

--- Player/IPlayerModel.cs
+++ Player/IPlayerModel.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+    AIM SD ASD 2020/2021 S2 project
+     
+    Project name: ASD-Project.
+ 
+    This file is created by team: 2.
+     
+    Goal of this file: changing the position of the player after input.
+     
+*/
 
 namespace Player
 {
