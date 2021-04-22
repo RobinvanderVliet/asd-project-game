8ba62ea0a93286ec996e9062a9256c32d2d3c855
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-04-22 09:48:29 +0200

    Added noiseGenerator class to genereate different map types

---
+++ WorldGeneration/NoiseMapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WorldGeneration
+{
+    public abstract class NoiseMapGenerator
+    {
+        public int[,] GenerateAreaMap(int size, int seed)
+        {
+            var noise = new FastNoiseLite();
+            noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
+            noise.SetSeed(seed);
+            noise.SetFrequency(0.015f);
+            noise.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
+            int[,] noiseData = new int[size,size];
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    noiseData[x,y] = (int)noise.GetNoise(x, y);
+                }
+                Console.WriteLine("");
+            }
+            return noiseData;
+        }
+    }
+}
