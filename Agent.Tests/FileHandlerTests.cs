10cc146eab9b92aed1792b0e60231178a357f1d6
jeroen <jerrik1998@gmail.com>
2021-04-22 11:13:11 +0200

    Initial tests and implementation

---
+++ Agent.Tests/FileHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Tests
+{
+    public class FileHandlerTests
+    {
+        static FileHandler sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //sut = new FileHandler();
+        }
+
+        [Test]
+        public void ImportFileTest()
+        {
+            var expected = "combat when player nearby player then attack combat";
+
+            //var result = sut.ImportFile(@"resource/import test file 1.txt");
+
+            //Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void ImportWrongFileTest()
+        {
+            //var result = sut.ImportFile(@"resource/import test file 1.php");
+
+            //Assert.Throws<Exception>(ImportFile);
+        }
+
+        [Test]
+        public void ExportFileTest()
+        {
+            var expected = "combat when player nearby player then attack combat";
+
+            //sut.exportFile("combat when player nearby player then attack combat");
+            //TODO get actual from file
+
+            //Assert.Equals(expected, actual);
+        }
+    }
+}
---
+++ Agent/FileHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent
+{
+    public class FileHandler
+    {
+        public string ImportFile(string filepath)
+        {
+            if (Path.GetExtension(filepath) != ".txt" || Path.GetExtension(filepath) != ".cfg") 
+            {
+                //TODO throw exception
+            }
+
+
+            FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+
+            try
+            {
+                string fileData = reader.ReadToEnd();
+
+                reader.Close();
+
+                return fileData;
+            }
+            //TODO add version to work with exception mapper
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ExportFile(string content)
+        {
+            //TODO change file name to selected by user
+            string tmpFileName = "agentFile.cfg";
+            string safeFileLocation = Path.Combine(Environment.CurrentDirectory, @"resource\", tmpFileName);
+
+            try
+            {
+                CreateDirectory(safeFileLocation);
+
+                FileStream fileStream = new FileStream(safeFileLocation, FileMode.Create, FileAccess.Write);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+
+                //TODO change to write with \n
+                streamWriter.Write(content);
+
+                streamWriter.Close();
+            }
+            //TODO change to work with exception handler
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void CreateDirectory(string filepath)
+        {
+            string directoryName = Path.GetDirectoryName(filepath);
+
+            if (directoryName.Length > 0)
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            if (!File.Exists(filepath))
+            {
+                File.Create(filepath);
+            }
+        }
+    }
+}
