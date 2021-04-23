c32d72796aea562335e000bbddd167b207574c77
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-04-23 11:40:01 +0200

    Added action interface

---
+++ Creature/Creature/States/Action/IAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creature.Creature.States.Action
+{
+    interface IAction
+    {
+        public void Do();
+        public void Do(object argument);
+    }
+}
