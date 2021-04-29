945e9697993ff266388292dc6a4450ffbcfe62bd
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-04-29 10:32:30 +0200

    delete tilemap tussen demo maps in

--- WorldGeneration/WorldGenerationComponent.cs
+++ WorldGeneration/WorldGenerationComponent.cs
@@ -6,11 +6,16 @@ namespace WorldGeneration
     {
         public Program()
         {
+            
+            var db = new Database.Database();
+            db.DeleteTileMap();
+            
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" En dan nu een random chunk met de seed " + 2243 + ": ");
             Console.ForegroundColor = ConsoleColor.Red;
             var map = new Map(120, 2243);
             map.LoadArea(new[] {0, 0}, 30);
+            db.DeleteTileMap();
             Console.ForegroundColor = ConsoleColor.White;
             var seed = new Random().Next(999999);
             Console.WriteLine(" En dan nu een random chunk met de seed " + seed + ": ");
@@ -18,8 +23,8 @@ namespace WorldGeneration
             var randomMap = new Map(120, seed);
             randomMap.LoadArea(new[] {0, 0}, 30);
             
-            var db = new Database.Database();
-            db.DeleteTileMap();
+            
+            
         }
     }
 }
