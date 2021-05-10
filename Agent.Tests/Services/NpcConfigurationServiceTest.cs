1a9520127db7f24a7283bf149f3d87d327f1ea13
Mark Brouwer <mbbrou@gmail.com>
2021-05-10 21:53:13 +0200

    Refactored NpcConfigurationService for anticipated flow for NPC configuration and added tests

--- Agent.Tests/Services/NpcConfigurationServiceTest.cs
+++ Agent.Tests/Services/NpcConfigurationServiceTest.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using Agent.exceptions;
 using Agent.Mapper;
 using Agent.Models;
 using Agent.Services;
 using Agent.Services.interfaces;
+using Moq;
 using NUnit.Framework;
 
 namespace Agent.Tests.Services
@@ -13,30 +15,84 @@ namespace Agent.Tests.Services
     [ExcludeFromCodeCoverage]
     public class NpcConfigurationServiceTest
     {
-        private FileToDictionaryMapper _mapper;
-        private INpcConfigurationService _npcConfigurationService;
-        private List<NpcConfiguration> _npcConfigurations;
+        private NpcConfigurationService _sut;
+        private Mock<FileHandler> _fileHandlerMock;
+        private Mock<Pipeline> _pipelineMock;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new NpcConfigurationService(new List<Configuration>(), new FileToDictionaryMapper());
+            _fileHandlerMock = new Mock<FileHandler>();
+            _sut._fileHandler = _fileHandlerMock.Object;
+            _pipelineMock = new Mock<Pipeline>();
+            _sut._pipeline = _pipelineMock.Object;
+        }
+        
+        [Test]
+        public void Test_CreateNewNpcConfiguration_WithNewNpc()
+        {
+            //Arrange
+            var filepath = String.Format(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\\..\\..\\"))) + "resource\\npcFileTest.txt";
+            
+            //Act
+            _sut.CreateConfiguration("TestNPCName", filepath);
+            
+            //Assert
+            Assert.True(_sut.GetConfigurations().Count > 0);
+            Assert.AreEqual(_sut.GetConfigurations()[0].GetSetting("aggressiveness"), "high");
+        }
 
-        // [SetUp]
-        // public void Setup()
-        // {
-        //     _mapper = new FileToDictionaryMapper();
-        //     _npcConfigurations = new List<NpcConfiguration>();
-        //     _npcConfigurationService = new NpcConfigurationService(_npcConfigurations, _mapper);
-        // }
-        //
-        // [Test]
-        // public void Test_CreateNewNpcConfiguration_WithNewNpc()
-        // {
-        //     //Arrange
-        //     var filepath = String.Format(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\\..\\..\\"))) + "resource\\npcFileTest.txt";
-        //     
-        //     //Act
-        //     _npcConfigurationService.CreateConfiguration("TestNPCName", filepath);
-        //     
-        //     //Assert
-        //     Assert.True(_npcConfigurationService.GetConfigurations().Count > 0);
-        //     Assert.AreEqual(_npcConfigurationService.GetConfigurations()[0].GetSetting("aggressiveness"), "high");
-        // }
+        [Test]
+        public void Test_Configure_CatchesSyntaxError()
+        {
+            //Arrange
+            Mock<ConsoleRetriever> mockedRetriever = new();
+            mockedRetriever.SetupSequence(x => x.GetConsoleLine()).Returns("zombie").Returns("incorrect:code").Returns("cancel");
+
+            _sut._consoleRetriever = mockedRetriever.Object;
+
+            //Act
+            _sut.Configure();
+
+            //Assert
+            Assert.AreEqual("missing '=' at 'code'", _sut.lastError);
+        }
+        
+        [Test]
+        public void Test_Configure_CatchesSemanticError()
+        {
+            //Arrange
+            var code = "explore=high";
+            var error = "Semantic error";
+            
+            Mock<ConsoleRetriever> mockedRetriever = new();
+            mockedRetriever.SetupSequence(x => x.GetConsoleLine()).Returns("zombie").Returns(code).Returns("cancel");
+            _sut._consoleRetriever = mockedRetriever.Object;
+            
+            _fileHandlerMock.Setup(x => x.ImportFile(It.IsAny<String>())).Returns(code);
+            _pipelineMock.Setup(x => x.CheckAst()).Throws(new SemanticErrorException(error));
+
+            //Act
+            _sut.Configure();
+
+            //Assert
+            Assert.AreEqual(error, _sut.lastError);
+        }
+        
+        [Test]
+        public void Test_Configure_SavesFileInNpcFolder()
+        {
+            Mock<ConsoleRetriever> mockedRetriever = new();
+            mockedRetriever.SetupSequence(x => x.GetConsoleLine()).Returns("zombie").Returns("aggressiveness=high");
+            
+            _sut._consoleRetriever = mockedRetriever.Object;
+            
+            //Act
+            _sut.Configure();
+            
+            //Assert
+            _fileHandlerMock.Verify( x => x.ExportFile(It.IsAny<String>(), It.IsAny<String>()), Times.Exactly(1));
+        }
     }
 }
--- Agent/Services/NpcConfigurationService.cs
+++ Agent/Services/NpcConfigurationService.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Agent.exceptions;
 using Agent.Mapper;
 using Agent.Models;
 using Agent.Services.interfaces;
+using Serilog;
 using Configuration = Agent.Models.Configuration;
 
 namespace Agent.Services
 {
     public class NpcConfigurationService : BaseConfigurationService
     {
+        private const string CANCEL_COMMAND = "cancel";
         private List<Configuration> _npcConfigurations;
-
+        public ConsoleRetriever _consoleRetriever;
+        public String lastError = "";
+        
         public NpcConfigurationService(List<Configuration> npcConfigurations, FileToDictionaryMapper fileToDictionaryMapper)
         {
             _npcConfigurations = npcConfigurations;
             _fileToDictionaryMapper = fileToDictionaryMapper;
+            _consoleRetriever = new ConsoleRetriever();
+            _fileHandler = new FileHandler();
+            _pipeline = new Pipeline();
         }
 
         public override void CreateConfiguration(string npcName, string filepath)
@@ -24,7 +33,6 @@ namespace Agent.Services
             npcConfiguration.Settings = _fileToDictionaryMapper.MapFileToConfiguration(filepath);
             
             _npcConfigurations.Add(npcConfiguration);
-            
         }
 
         public override List<Configuration> GetConfigurations()
@@ -32,9 +40,43 @@ namespace Agent.Services
             return _npcConfigurations;
         }
 
-        public override void StartConfiguration(ConfigurationType configurationType)
+        public override void Configure()
         {
+            //TODO: Seems like duplicate code for now, but must be refactored later to match anticipated feature 'Configure NPC during a game'
+            Console.WriteLine("What NPC do you wish to configure?");
+            var npc = _consoleRetriever.GetConsoleLine();
             
+            if (npc.Equals(CANCEL_COMMAND))
+            {
+                return;
+            }
+            
+            Console.WriteLine("Please provide code for the NPC");
+            var code = _consoleRetriever.GetConsoleLine();
+            
+            
+            
+            try
+            {
+                _pipeline.ParseString(code);
+                _pipeline.CheckAst();
+                var output = _pipeline.GenerateAst();
+
+                string fileName = "npc\\" + npc + "-config.cfg";
+                _fileHandler.ExportFile(output, fileName);
+            }
+            catch (SyntaxErrorException e)
+            {
+                lastError = e.Message;
+                Log.Logger.Information("Syntax error: " + e.Message);
+                Configure();
+            }
+            catch (SemanticErrorException e)
+            {
+                lastError = e.Message;
+                Log.Logger.Information("Semantic error: " + e.Message);
+                Configure();
+            }
         }
     }
 }
