fd218debbcb6513c259a8f7b0dfdfcf1c3ab63ac
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-05-11 16:29:53 +0200

    Fixed dependency to using instead of direct reference

--- Creature/Creature/StateMachine/Data/ICreatureData.cs
+++ Creature/Creature/StateMachine/Data/ICreatureData.cs
@@ -1,11 +1,12 @@
 ﻿using Creature.World;
+using System.Numerics;
 
 namespace Creature.Creature.StateMachine.Data
 {
     public interface ICreatureData
     {
         bool IsAlive { get; }
-        System.Numerics.Vector2 Position { get; set; }
+        Vector2 Position { get; set; }
         int VisionRange { get; set; }
         int Damage { get; set; }
         double Health { get; set; }
