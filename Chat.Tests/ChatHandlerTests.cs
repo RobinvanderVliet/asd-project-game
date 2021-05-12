64b6b2bb270b0a34715fcd2c1ed8d9f5d52b3f96
Jordi Stevens <JSA.Stevens@student.han.nl>
2021-05-12 15:45:38 +0200

    Auto stash before merge of "sub-task/combining-feature-24-25" and "origin/sub-task/combining-feature-24-25"

---
+++ Chat.Tests/ChatHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
+using Moq;
+using Network;
+
+namespace Chat.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class ChatHandlerTests
+    {
+        //Declaration and initialisation of constant variables
+        private ChatHandler _chatHandler;
+        //Declaration of variables
+
+
+        //Declaration of mocks
+        private Mock<ClientController> _mockedClientController;
+   
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockedClientController = new Mock<ClientController>();
+            _chatHandler = new ChatHandler(_mockedClientController.Object);
+            
+        }
+
+        [Test]
+        public void Test_SendSay_SendsTheMessageAndPacketTypeToClientController()
+        {
+            //Arrange ---------
+            string message = "Hello World";
+            _mockedClientController.Setup(mock => mock.SendPayload(message, PacketType.Chat));
+
+            //Act ---------
+            _chatHandler.SendSay(message);
+            //Assert ---------
+            _mockedClientController.Verify(mock => mock.SendPayload(message, PacketType.Chat), Times.Once());
+        }
+
+        [Test]
+        public void Test_SendShout_SendsTheMessageAndPacketTypeToClientController()
+        {
+            //Arrange ---------
+            string message = "Hello World";
+            _mockedClientController.Setup(mock => mock.SendPayload(message, PacketType.Chat));
+
+            //Act ---------
+            _chatHandler.SendShout(message);
+            //Assert ---------
+            _mockedClientController.Verify(mock => mock.SendPayload(message, PacketType.Chat), Times.Once());
+        }
+    }
+}
