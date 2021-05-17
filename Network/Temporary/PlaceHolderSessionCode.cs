f5e717c23ceaea5eea6447ce16ce94f0052a9ff1
Wern212 <wdj.vanvoorst@student.han.nl>
2021-05-17 16:14:49 +0200

    Added some placeholder code to communicate ideas about how to share the session seed.

---
+++ Network/Temporary/PlaceHolderSessionCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using WorldGeneration;
+
+namespace Network.Temporary
+
+// This is a temporary file to use as placeholder since we do not have access to the full session code yet. This code WILL be deleted after the merge with session code, you have been warned.
+{
+    public class PlaceHolderSessionCode
+    {
+        
+        // The following code should end up in Sessions.
+        
+        private int _sessionSeed;
+        private List<string> joinedClients;
+        
+        private void GenerateSessionSeed()
+        {
+            // This function is called when a new session is started to set the seed of that session.
+            _sessionSeed = new Random().Next(1, 999999);
+            foreach (var client in joinedClients)
+            {
+                sendPackage(client, _sessionSeed);
+            }
+        }
+
+        private void sendPackage(string client, int sessionSeed)
+        {
+            throw new NotImplementedException();
+        }
+
+        // The following code should end up at the receiving end of the messages:
+        
+        private Map gameMap;
+
+        private void SetMapSeed(int seed)
+        {
+            gameMap = MapFactory.GenerateMap(seed: seed);
+        }
+        
+
+    }
+}
