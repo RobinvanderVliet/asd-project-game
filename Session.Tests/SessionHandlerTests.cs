e112d7c586461342b97653ab128e90b858f7a4b3
Pepijn van Erp <ppb.vanerp@student.han.nl>
2021-05-17 10:22:11 +0200

    Created Test templates for all functions in SessionHandler

--- Session.Tests/SessionHandlerTests.cs
+++ Session.Tests/SessionHandlerTests.cs
@@ -13,17 +13,42 @@ namespace Session.Tests
         SessionHandler _sessionHandler;
 
         //Declaration of mocks
-        private Mock<IClientController> _mockedClientController;
+        private Mock<ClientController> _mockedClientController;
         private Mock<Session> _session;
-
 
         [SetUp]
         public void Setup()
         {
+            _mockedClientController = new Mock<ClientController>();
+            _session = new Mock<Session>();
         }
 
         [Test]
-        public void Test1()
+        public void Test_JoinSession()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_CreateSession()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_RequestSessions()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_SendSessionDTO()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_HandlePacket()
         {
             Assert.Pass();
         }
