a03510e913283628f9ba146c34c259f002ed2547
Wim Beukers <W.Beukers@student.han.nl>
2021-05-17 21:52:37 +0200

    Excluded classes without logic

--- Network/PacketBuilder.cs
+++ Network/PacketBuilder.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Network
 {
+    [ExcludeFromCodeCoverage]
     public class PacketBuilder
     {
         PacketHeaderDTO packetHeaderDTO = new PacketHeaderDTO();
