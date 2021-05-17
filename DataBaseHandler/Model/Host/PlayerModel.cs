c31617223dc6d7ed6b55e5226d9dd685a26df3c7
Robin Magdelijns <r.magdelijns@gmail.com>
2021-05-17 14:32:01 +0200

    Added first classes for LiteDB

---
+++ DataBaseHandler/Model/Host/PlayerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LiteDB;
+
+namespace DataBaseHandler.Model.Host
+{
+    public class PlayerModel
+    {
+        public MainGameModel GameGuid { get; set; }
+        public Guid PlayerGuid { get; set; }
+
+
+        [BsonCtor]
+        public PlayerModel(Guid playerGuid, MainGameModel gameGuid)
+        {
+            playerGuid = playerGuid;
+            GameGuid = gameGuid;
+
+        }
+        
+    }
+    
+}
+
