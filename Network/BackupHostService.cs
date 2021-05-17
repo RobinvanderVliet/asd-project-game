07ea263d8ce59e5429a8f89a1fe5953f48d74149
Vincent v.d Berg <VFG.vandenBerg@student.han.nl>
2021-05-17 16:19:09 +0200

    Create BackupHostService.cs

---
+++ Network/BackupHostService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    class BackupHostService 
+    {
+        private Boolean _isBackupHost;
+
+        //database shit
+        private Database database;
+
+        public BackupHostService() 
+        {
+            _isBackupHost = false;
+        }
+
+        public void UpdateBackupDatabase(DatabaseModel data)
+        {
+            try
+            {
+                if (CheckExists(data))
+                {
+                    //todo update database
+                }
+                else
+                {
+                    AddToBackupDatabase(data);
+                }
+
+                //save in database
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some data did not save succesfull, please contact your host!");
+            }
+
+        }
+
+        private void AddToBackupDatabase(DatabaseModel data) {
+        //todo call add function
+        }
+
+        private Boolean CheckExists(DatabaseModel data)
+        {
+            return false; //todo
+        }
+
+        public Boolean IsBackupHost
+        {
+            get => _isBackupHost; set => _isBackupHost = value;
+        }
+    }
+}
