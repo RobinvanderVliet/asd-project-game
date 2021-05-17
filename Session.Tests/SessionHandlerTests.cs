21f3e834bbd8b20a2c322319eaa46e6818899ffe
Pepijn van Erp <ppb.vanerp@student.han.nl>
2021-05-17 15:56:32 +0200

    Created request sessions test and failure test for joining a session

--- Session.Tests/SessionHandlerTests.cs
+++ Session.Tests/SessionHandlerTests.cs
@@ -1,5 +1,6 @@
 using Moq;
 using Network;
+using Network.DTO;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using Session.DTO;
@@ -12,7 +13,9 @@ namespace Session.Tests
     public class SessionHandlerTests
     {
         //Declaration and initialisation of constant variables
-        SessionHandler _sessionHandler;
+        private SessionHandler _sessionHandler;
+
+        private PacketDTO _packetDTO;
 
         //Declaration of mocks
         private Mock<IClientController> _mockedClientController;
@@ -22,6 +25,7 @@ namespace Session.Tests
         {
             _mockedClientController = new Mock<IClientController>();
             _sessionHandler = new SessionHandler(_mockedClientController.Object);
+            _packetDTO = new PacketDTO();
         }
 
         // TODO: fix this test
@@ -29,10 +33,10 @@ namespace Session.Tests
         public void Test_JoinSession()
         {
             // Arrange ------------
-            string testSessionId = "testSessionId";
+            //string testSessionId = "testSessionId";
 
             // Act ----------------
-            _sessionHandler.JoinSession(testSessionId);
+            //_sessionHandler.JoinSession(testSessionId);
 
             // Assert -------------
             //_mockedClientController.Verify(mock => mock.SetSessionId(testSessionId), Times.Once());
@@ -57,22 +61,75 @@ namespace Session.Tests
         [Test]
         public void Test_RequestSessions()
         {
-            //Arrange ---------
+            // Arrange ---------
             SessionDTO sessionDTO = new SessionDTO(SessionType.RequestSessions);
             var payload = JsonConvert.SerializeObject(sessionDTO);
 
             _mockedClientController.Setup(mock => mock.SendPayload(payload, PacketType.Session));
 
-            //Act ---------
+            // Act ---------
             _sessionHandler.RequestSessions();
 
-            //Assert ---------
+            // Assert ---------
             _mockedClientController.Verify(mock => mock.SendPayload(payload, PacketType.Session), Times.Once());
         }
 
         [Test]
-        public void Test_HandlePacket()
+        public void Test_HandlePacketRequestSessions()
         {
+            // Arrange ---------
+            _sessionHandler.CreateSession("testSessionName");
+
+            SessionDTO sessionDTO = new SessionDTO(SessionType.RequestSessions);
+            var payload = JsonConvert.SerializeObject(sessionDTO);
+            _packetDTO.Payload = payload;
+            PacketHeaderDTO packetHeaderDTO = new PacketHeaderDTO();
+            packetHeaderDTO.OriginID = "testOriginId";
+            packetHeaderDTO.SessionID = null;
+            packetHeaderDTO.PacketType = PacketType.Session;
+            packetHeaderDTO.Target = "host";
+            _packetDTO.Header = packetHeaderDTO;
+
+            // Act -------------
+            HandlerResponseDTO actualResult = _sessionHandler.HandlePacket(_packetDTO);
+
+            // Assert ----------
+            HandlerResponseDTO expectedResult = new HandlerResponseDTO(true, "testSessionName");
+            Assert.AreEqual(expectedResult.ReturnToSender, actualResult.ReturnToSender);
+        }
+
+        [Test]
+        public void Test_HandlePacketRequestToJoinSessionThatDoesNotExist()
+        {
+            // Arrange ---------
+            _sessionHandler.CreateSession("testSessionName");
+
+            SessionDTO sessionDTO = new SessionDTO(SessionType.RequestToJoinSession);
+            var payload = JsonConvert.SerializeObject(sessionDTO);
+            _packetDTO.Payload = payload;
+            PacketHeaderDTO packetHeaderDTO = new PacketHeaderDTO();
+            packetHeaderDTO.OriginID = "testOriginId";
+            packetHeaderDTO.SessionID = null;
+            packetHeaderDTO.PacketType = PacketType.Session;
+            packetHeaderDTO.Target = "host";
+            _packetDTO.Header = packetHeaderDTO;
+
+            // Act -------------
+            HandlerResponseDTO actualResult = _sessionHandler.HandlePacket(_packetDTO);
+
+            // Assert ----------
+            HandlerResponseDTO expectedResult = new HandlerResponseDTO(false, "testSessionName");
+            Assert.AreEqual(expectedResult.ReturnToSender, actualResult.ReturnToSender);
+        }
+
+        [Test]
+        public void Test_HandlePacketRequestToJoinSession()
+        {
+            // Arrange ---------
+
+            // Act -------------
+
+            // Assert ----------
             Assert.Pass();
         }
     }
