e3c4df92e2d46fd668fb95ac431b7459782cbcfd
R-Gerrits <48597297+R-Gerrits@users.noreply.github.com>
2021-05-18 16:26:00 +0200

    Added message when creating an Agent for prototyping

--- Creature/Creature/Player.cs
+++ Creature/Creature/Player.cs
@@ -1,13 +1,14 @@
 ﻿using Creature.Creature.StateMachine;
 using Creature.Creature.StateMachine.CustomRuleSet;
 using Creature.Creature.StateMachine.Data;
+using Network;
 
 namespace Creature
 {
     public class Player : ICreature
     {
         private PlayerStateMachine _playerStateMachine;
-
+        private ClientController _clientController;
         public ICreatureStateMachine CreatureStateMachine
         {
             get => _playerStateMachine;
@@ -15,7 +16,9 @@ namespace Creature
 
         public Player(PlayerData playerData, RuleSet ruleSet)
         {
+            _clientController = new ClientController(new NetworkComponent());
             _playerStateMachine = new(playerData, ruleSet);
+            SendChatMessenge("Agent is starting to replace player");
             _playerStateMachine.StartStateMachine();
         }
 
@@ -27,6 +30,11 @@ namespace Creature
         public void HealAmount(double amount)
         {
             _playerStateMachine.CreatureData.Health += amount;
+        }
+
+        public void SendChatMessenge(string message)
+        {
+            _clientController.SendPayload(message, PacketType.Chat);
         }
     }
 }
