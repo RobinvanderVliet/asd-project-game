96358a27daccc4f3750ef567c79ae81cdf295f04
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-05-20 12:27:47 +0200

    Excluded heartbeatdto from coverage

--- Network/DTO/HeartbeatDTO.cs
+++ Network/DTO/HeartbeatDTO.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Network.DTO
 {
+    [ExcludeFromCodeCoverage]
     class HeartbeatDTO
     {
         public string sessionID {get; set;}
