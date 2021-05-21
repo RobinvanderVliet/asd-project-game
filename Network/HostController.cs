01841263aef939a234600db2eaf6ff4577369686
Danny Hageman <DHM.Hageman@student.han.nl>
2021-05-21 16:23:00 +0200

    Merge fixes

--- Network/HostController.cs
+++ Network/HostController.cs
@@ -19,7 +19,7 @@ namespace Network
 
         public void ReceivePacket(PacketDTO packet)
         {
-            if(packet.Header.SessionID == _sessionId || packet.Header.PacketType == PacketType.Session || packet.Header.PacketType == PacketType.GameSession)
+            if(packet.Header.SessionID == _sessionId || packet.Header.PacketType == PacketType.Session)
             {
                 HandlePacket(packet);
             }
