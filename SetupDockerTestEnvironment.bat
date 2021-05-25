e37e08e1df2e191f7760cff4a35db960220c2c08
Jordi Stevens <JSA.Stevens@student.han.nl>
2021-05-25 22:32:08 +0200

    Script verbeterd zodat het de netwerkswitch skipped wanneer die al bestaat

--- SetupDockerTestEnvironment.bat
+++ SetupDockerTestEnvironment.bat
@@ -12,22 +12,31 @@ IF NOT EXIST "ASD-Game" (
  pause
  exit
 )
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker container inspect -f '{{.State.Status}}' network-switch-dotnet-websocketsharp`) DO (
+SET var=%%F
+)
+IF NOT %var%! == 'running'! (
 IF NOT EXIST "../asd-project-networkhandler/.net prototypes/NetworkSwitch WebSocketsSharp/NetworkSwitch" (
  echo The folder ../asd-project-networkhandler/.net prototypes/NetworkSwitch WebSocketsSharp/NetworkSwitch does not exist. Can not continue with the script.
  pause
  exit
 )
+ docker rm -f network-switch-dotnet-websocketsharp
+ docker rmi -f network-switch-dotnet-websocketsharp
+ echo Publishing NetworkSwitch!
+ cd ../asd-project-networkhandler/.net prototypes/NetworkSwitch WebSocketsSharp/NetworkSwitch
+ dotnet publish -c Release 
+ echo Building NetworkSwitch Docker Image!
+ docker build -t network-switch-dotnet-websocketsharp .
+ docker run -itd -e PORT=8088 -p 0.0.0.0:8088:8088 --name network-switch-dotnet-websocketsharp network-switch-dotnet-websocketsharp
+) else (
+echo Network switch is already running
+)
 
 echo Removing old containers and images!
-docker rm -f network-switch-dotnet-websocketsharp asd-game-host asd-game-client
-docker rmi -f network-switch-dotnet-websocketsharp asd-game
-
-echo Publishing NetworkSwitch!
-cd ../asd-project-networkhandler/.net prototypes/NetworkSwitch WebSocketsSharp/NetworkSwitch
-dotnet publish -c Release 
-echo Building NetworkSwitch Docker Image!
-docker build -t network-switch-dotnet-websocketsharp .
-
+docker rm -f asd-game-host asd-game-client
+docker rmi -f asd-game
 
 cd %~dp0
 echo Publishing ASD-Game!
@@ -37,7 +46,6 @@ echo Building ASD-Game Docker Image!
 docker build -t asd-game .
 
 echo Running images in containers!
-docker run -itd -e PORT=8088 -p 0.0.0.0:8088:8088 --name network-switch-dotnet-websocketsharp network-switch-dotnet-websocketsharp
 docker run -itd -e TEST=HOST --name asd-game-host asd-game
 docker run -itd -e TEST=CLIENT --name asd-game-client asd-game
 
