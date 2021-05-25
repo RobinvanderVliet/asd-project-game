97413e2c146385add9b46b716f51c024dda02951
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-05-25 17:02:28 +0200

    Added service for agent

---
+++ Creature/Services/AgentService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Creature.Services
+{
+    public class AgentService : IAgentService
+    {
+        private Dictionary<string, ICreature> agents;
+
+        public AgentService(string playerId)
+        {
+            
+        }
+        
+        public void Activate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeActivate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void IsActivated()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Creature/Services/IAgentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Creature.Services
+{
+    public interface IAgentService
+    {
+        public void Activate();
+        public void DeActivate();
+        public void IsActivated();
+    }
+}
