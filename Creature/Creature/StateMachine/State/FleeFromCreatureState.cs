d90d53093cb6851793adc37daaedae41a33fc82b
JW.vanRijswijk <jw.vanrijswijk@student.han.nl>
2021-05-26 19:34:22 +0200

    Added simple implementation of FleeFromPlayerState

---
+++ Creature/Creature/StateMachine/State/FleeFromCreatureState.cs
@@ -0,0 +1,36 @@
+﻿using Creature.Creature.StateMachine.Data;
+using System;
+using System.Numerics;
+
+namespace Creature.Creature.StateMachine.State
+{
+    public class FleeFromCreatureState : CreatureState
+    {
+        public FleeFromCreatureState(ICreatureData creatureData) : base(creatureData)
+        {
+            _creatureData = creatureData;
+        }
+
+        public override void Do()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Do(ICreatureData creatureData)
+        {
+            ICreatureData playerData = creatureData;
+
+            Vector2 newMonsterPosition = _creatureData.Position;
+            if (Vector2.DistanceSquared(_creatureData.Position, playerData.Position) <= Vector2.DistanceSquared(new Vector2(_creatureData.Position.X + 1, _creatureData.Position.Y + 1), playerData.Position))
+            {
+                newMonsterPosition = new Vector2(_creatureData.Position.X + 1, _creatureData.Position.Y + 1);
+            }
+            else if (Vector2.DistanceSquared(_creatureData.Position, playerData.Position) <= Vector2.DistanceSquared(new Vector2(_creatureData.Position.X - 1, _creatureData.Position.Y - 1), playerData.Position))
+            {
+                newMonsterPosition = new Vector2(_creatureData.Position.X - 1, _creatureData.Position.Y - 1);
+            }
+
+            _creatureData.Position = newMonsterPosition;
+        }
+    }
+}
