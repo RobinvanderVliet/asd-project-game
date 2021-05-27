2acb21cc20096c0c52cc21763d992c43e28bcd06
jeroen <jerrik1998@gmail.com>
2021-05-27 11:36:56 +0200

    Created monster difficulty as far as possible
    Still needs screen and list of monsters from world generation

---
+++ UserInterface/GameConfiguration/GameConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface.GameConfiguration
+{
+    class GameConfiguration
+    {
+        private MonsterDifficulty _newMonsterDifficulty;
+        private MonsterDifficulty _currentMonsterDifficulty;
+
+        public GameConfiguration(MonsterDifficulty monsterDifficulty)
+        {
+            _newMonsterDifficulty = monsterDifficulty;
+            _currentMonsterDifficulty = MonsterDifficulty.Medium;
+        }
+
+        public void SetDifficulty(MonsterDifficulty monsterDifficulty) 
+        {
+            _currentMonsterDifficulty = _newMonsterDifficulty;
+            _newMonsterDifficulty = monsterDifficulty;
+        }
+
+        //Moet aangeroepen worden tijdens creatie monsters en bij aanpas command
+        public int AdjustMonsterValue(int monsterProperty) 
+        {
+            return monsterProperty / (int)_currentMonsterDifficulty * (int)_newMonsterDifficulty;
+            
+        }
+
+
+    }
+}
---
+++ UserInterface/GameConfiguration/MonsterDifficulty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface.GameConfiguration
+{
+    public enum MonsterDifficulty
+    {
+        Easy = 50,
+        Medium = 100,
+        Hard = 200,
+        Impossible = 500
+    }
+}
