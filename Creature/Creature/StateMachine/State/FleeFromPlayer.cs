8606e69c2ef47d80408141be314a0fe395c6c81a
R-Gerrits <48597297+R-Gerrits@users.noreply.github.com>
2021-05-27 10:48:05 +0200

    Do not delete my stuff Jonathan

---
+++ Creature/Creature/StateMachine/State/FleeFromPlayer.cs
@@ -0,0 +1,17 @@
+﻿using Creature.Creature.StateMachine.Data;
+
+namespace Creature.Creature.StateMachine.State
+{
+    public class FleeFromPlayer : CreatureState
+    {
+        public FleeFromPlayer(ICreatureData creatureData) : base(creatureData)
+        {
+            _creatureData = creatureData;
+        }
+
+        public override void Do(ICreatureData creatureData)
+        {
+            
+        }
+    }
+}
