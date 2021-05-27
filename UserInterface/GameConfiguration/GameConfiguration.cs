fb4646434ec98067da143de0a55a1974da9cf38e
jeroen <jerrik1998@gmail.com>
2021-05-27 12:01:40 +0200

    Ask question list start made

--- UserInterface/GameConfiguration/GameConfiguration.cs
+++ UserInterface/GameConfiguration/GameConfiguration.cs
@@ -17,6 +17,44 @@ namespace UserInterface.GameConfiguration
             _currentMonsterDifficulty = MonsterDifficulty.Medium;
         }
 
+        public void AskUserForMonsterDifficulty() 
+        {
+            try
+            {
+                //Change to work with UI when implemented
+                Console.WriteLine("Welke game moeilijkheidsgraad wilt u?");
+                Console.WriteLine("1. Easy");
+                Console.WriteLine("2. Medium");
+                Console.WriteLine("3. Hard");
+                Console.WriteLine("4. Impossible");
+                Console.WriteLine("Voer het nummer van uw keuze in!");
+                string userInput = Console.ReadLine();
+                userInput = userInput.Trim(',', ' ');
+                int userChoice = int.Parse(userInput);
+
+                switch (userChoice)
+                {
+                    case 1:
+                        _newMonsterDifficulty = MonsterDifficulty.Easy;
+                        break;
+                    case 2:
+                        _newMonsterDifficulty = MonsterDifficulty.Medium;
+                        break;
+                    case 3:
+                        _newMonsterDifficulty = MonsterDifficulty.Hard;
+                        break;
+                    case 4:
+                        _newMonsterDifficulty = MonsterDifficulty.Impossible;
+                        break;
+                }
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("Aub selecteer een van de opties, nummers zijn genoeg");
+                AskUserForMonsterDifficulty();
+            }
+        }
+
         public void SetDifficulty(MonsterDifficulty monsterDifficulty) 
         {
             _currentMonsterDifficulty = _newMonsterDifficulty;
