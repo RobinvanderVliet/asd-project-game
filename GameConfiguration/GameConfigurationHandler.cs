da753581a3d6f6d36c5879978d2d811893ffe951
Jesper Jansen <jesper.jansen2000@gmail.com>
2021-05-27 09:35:42 +0200

    Added world configuration class

---
+++ GameConfiguration/GameConfigurationHandler.cs
@@ -0,0 +1,12 @@
+namespace GameConfiguration
+{
+    public class GameConfigurationHandler
+    {
+        public void changeConfiguration()
+        {
+            //To do: functie die de configuratie form tekent
+            //To do: functie die elke configuratie aanpast
+            //To do: default configuration maken bij geen keuze
+        }
+    }
+}
