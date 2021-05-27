7a81c42d15c40f715d014302e07aac4caf3ed1b8
Wern212 <wdj.vanvoorst@student.han.nl>
2021-05-27 15:27:59 +0200

    Added EastOnly, WestOnly, NorthOnly, Southonly

--- WorldGeneration/RoadPresetFactory.cs
+++ WorldGeneration/RoadPresetFactory.cs
@@ -88,8 +88,111 @@ namespace WorldGeneration
             return roadmap;
         }
         
+        private static ITile[] EastOnly (int chunksize)
+        {
+            // First, if there is an even number of tiles in a chunk, we need two rows of roads. Otherwise, we'll only use one.
+            ITile[] roadmap = new ITile[chunksize * chunksize];
+            if (chunksize % 2 == 0)
+            { 
+                // Example using chunksize of 8:
+                // half of chunksize = 4;
+                // 3 * 8 = 24. Because we start at 0, this is the first tile of the fourth row.
+                // Instead of filling the next two rows, only fill the first half of both rows. The easiest ways to do this is do two loops.
+                int halfwayinthechunk = ((chunksize /2)-1) * chunksize;
+                for (int i = halfwayinthechunk; i < halfwayinthechunk + (chunksize/2) ; i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+                for (int i = halfwayinthechunk + chunksize; i < halfwayinthechunk + chunksize + (chunksize/2) ; i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+            }
+            else
+            {
+                // Simple example using chunksize of 9:
+                // half of chunksize = 4;
+                // 4 * 9 = 36. This is the first tile of the fifth row.
+                // Instead of filling the entire row, just fill the first half +1.
+                int halfwayinthechunk = ((chunksize -1) / 2) * chunksize;
+                for (int i = halfwayinthechunk; i < halfwayinthechunk + ((chunksize +1)/2) ; i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+            }
+            return roadmap;
+        }
         
-        
-        
+        private static ITile[] WestOnly (int chunksize)
+        {
+            // First, if there is an even number of tiles in a chunk, we need two rows of roads. Otherwise, we'll only use one.
+            ITile[] roadmap = new ITile[chunksize * chunksize];
+            if (chunksize % 2 == 0)
+            {
+                // Uses similar logic to EastOnly, but instead of filling the first half of the row, fill the second half.
+                int halfwayinthechunk = ((chunksize /2)-1) * chunksize;
+                for (int i = halfwayinthechunk + (chunksize/2); i < halfwayinthechunk + chunksize; i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+                for (int i = halfwayinthechunk + chunksize + (chunksize/2); i < halfwayinthechunk + (chunksize*2); i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+            }
+            else
+            {
+                int halfwayinthechunk = ((chunksize -1) / 2) * chunksize;
+                for (int i = halfwayinthechunk + ((chunksize -1)/2); i < halfwayinthechunk + chunksize  ; i++)
+                {
+                    roadmap[i] = new StreetTile(0, 0);
+                }
+            }
+            return roadmap;
+        }
+        private static ITile[] NorthOnly (int chunksize)
+        {
+            // First, if there is an even number of tiles in a chunk, we need two rows of roads. Otherwise, we'll only use one.
+            ITile[] roadmap = new ITile[chunksize * chunksize];
+            if (chunksize % 2 == 0)
+            {
+                for (int i = (chunksize/2)-1; i < (chunksize* chunksize)/2; i += chunksize)
+                {
+                    roadmap[i] = new StreetTile(0,0);
+                    roadmap[i + 1] = new StreetTile(0, 0);
+                }
+            }
+            else
+            {
+                for (int i = (chunksize-1)/2; i < (chunksize*chunksize)/2; i += chunksize)
+                {
+                    roadmap[i] = new StreetTile(0,0);
+                }
+            }
+            
+            return roadmap;
+        }
+        private static ITile[] SouthOnly (int chunksize)
+        {
+            // First, if there is an even number of tiles in a chunk, we need two rows of roads. Otherwise, we'll only use one.
+            ITile[] roadmap = new ITile[chunksize * chunksize];
+            if (chunksize % 2 == 0)
+            {
+                for (int i = (chunksize * chunksize/2) + (chunksize/2)-1; i < ((chunksize*chunksize)-1); i += chunksize)
+                {
+                    roadmap[i] = new StreetTile(0,0);
+                    roadmap[i + 1] = new StreetTile(0, 0);
+                }
+            }
+            else
+            {
+                for (int i = (chunksize * chunksize/2) + ((chunksize-1)/2); i < (chunksize*chunksize)-1; i += chunksize)
+                {
+                    roadmap[i] = new StreetTile(0,0);
+                }
+            }
+            
+            return roadmap;
+        }
     }
 }
