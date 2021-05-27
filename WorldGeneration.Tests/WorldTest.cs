402f9611d88cf014053bfd53df2495a64a66319d
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-05-27 16:57:49 +0200

    worldTestSetup

--- WorldGeneration.Tests/WorldTest.cs
+++ WorldGeneration.Tests/WorldTest.cs
@@ -1,7 +1,32 @@
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
 namespace WorldGeneration.Tests
 {
+    [ExcludeFromCodeCoverage]  
+    [TestFixture]
     public class WorldTest
     {
+        //Declaration and initialisation of constant variables
+ 
+        //Declaration of variables
+ 
+        //Declaration of mocks
+ 
+        [SetUp]
+        public void Setup()
+        {
+            //Initialisation of variables
+            //Initialisation of mocks
+        }
         
+        [Test]
+        public void Test_Function_DoesThing() 
+        {
+            //Arrange ---------
+            //Act ---------
+            //Assert ---------
+            Assert.That(true);
+        }
     }
 }
