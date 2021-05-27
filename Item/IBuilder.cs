3e37941692dcd623b0f1da70e6983fc413d217f5
Abdul AlZuabi <A.Alzuabi@student.han.nl>
2021-05-27 07:14:30 +0200

    feat: added Weaponbuilder and Builder

---
+++ Item/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Player.Model.ItemStats;
+
+namespace Player.Model
+{
+    public interface IBuilder
+    {
+        public void Reset();
+        public void SetName(string name);
+        public void SetDescription(string description);
+        public void SetRarity(Rarity rarity);
+    }
+}
---
+++ Item/Weapon/WeaponBuilder.cs
@@ -0,0 +1,61 @@
+﻿using Player.Model;
+using Player.Model.ItemStats;
+using Player.Model.Weapon.WeaponStats;
+
+namespace Weapon
+{
+    public class WeaponBuilder : IBuilder
+    {
+        private Weapon _weapon;
+
+        private WeaponBuilder()
+        {
+            _weapon = new Weapon();
+        }
+
+        public void SetName(string name)
+        {
+            _weapon.ItemName = name;
+        }
+
+        public void SetDescription(string description)
+        {
+            _weapon.Description = description;
+        }
+
+        public void SetRarity(Rarity rarity)
+        {
+            _weapon.Rarity = rarity;
+        }
+
+        public void SetDamage(WeaponDamage damage)
+        {
+            _weapon.Damage = damage;
+        }
+
+        public void SetWeaponDistance(WeaponDistance weaponDistance)
+        {
+            _weapon.Distance = weaponDistance;
+        }
+
+        public void SetWeaponSpeed(WeaponSpeed speed)
+        {
+            _weapon.Speed = speed;
+        }
+        
+        public void SetWeaponType(WeaponType type)
+        {
+            _weapon.Type = type;
+        }
+        
+        public void Reset()
+        {
+            _weapon = new Weapon();
+        }
+        
+        public Weapon GetItem()
+        {
+            return _weapon;
+        }
+    }
+}
