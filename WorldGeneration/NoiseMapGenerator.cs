67ab4b1e0c0d5be63a38e534254829b2ea833f3f
Wern212 <wdj.vanvoorst@student.han.nl>
2021-05-31 11:00:08 +0200

    Added Roads to NoiseMapGenerator.cs

--- WorldGeneration/NoiseMapGenerator.cs
+++ WorldGeneration/NoiseMapGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using WorldGeneration.Models;
+using WorldGeneration.Models.Enums;
 using WorldGeneration.Models.HazardousTiles;
 using WorldGeneration.Models.Interfaces;
 using WorldGeneration.Models.TerrainTiles;
@@ -28,24 +29,71 @@ namespace WorldGeneration
             {
                 SetupNoise(seed);
             }
-            var map = new ITile[chunkRowSize * chunkRowSize];
             // check which edges of the chunk will have roads.
+            CompassDirections compassDirection = GetCompassDirection(chunkX, chunkY);
+            
+            // Create a map that has the roads, but these roads are missing coordinates.
+            var map = RoadPresetFactory.GetRoadPreset(chunkRowSize, compassDirection);
+            
             for (var y = 0; y < chunkRowSize; y++)
             {
                 for (var x = 0; x < chunkRowSize; x++)
                 {
-                    // IF in roadmap, add road, ELSE continue below.
-                    map[y * chunkRowSize + x] = GetTileFromNoise(noise.GetNoise(x + chunkX * chunkRowSize, y + chunkY * chunkRowSize)
-                        , x + chunkRowSize * chunkX
-                        , chunkRowSize * chunkY - chunkRowSize + y);
+                    if (map[y * chunkRowSize + x].GetType() == typeof(StreetTile))
+                    {
+                        map[y * chunkRowSize + x] = new StreetTile(x, y);
+                    }
+                    else
+                    {
+                        // IF in roadmap, add road, ELSE continue below.
+                        map[y * chunkRowSize + x] = GetTileFromNoise(
+                            noise.GetNoise(x + chunkX * chunkRowSize, y + chunkY * chunkRowSize)
+                            , x + chunkRowSize * chunkX
+                            , chunkRowSize * chunkY - chunkRowSize + y);
+                    }
                 }
             }
             return new Chunk(chunkX, chunkY, map, chunkRowSize);
         }
 
-        private ITile GetTileFromNoise(float noise, int x, int y)
+        private CompassDirections GetCompassDirection (int chunkX, int chunkY)
         {
-            return (noise * 10) switch
+            // Step 1: generate four binaries based on the compass directions.
+            // These use the coordinate of the current chunk and the adjacent chunk, so when the calculation is repeated in the adjacent chunk it will result in the same result.
+            int north = GetBinaryForRoads(chunkY + chunkY + 1);
+            int south = GetBinaryForRoads(chunkY + chunkY - 1);
+            int east = GetBinaryForRoads(chunkX + chunkX + 1);
+            int west = GetBinaryForRoads(chunkX + chunkX - 1);
+            
+            // Step 2: based on the previous binaries, a switch case is used to return the correct compass direction.
+            switch (north, south, east, west)
+            {
+                case (0, 0, 0, 0):
+                    return CompassDirections.NoRoads;
+                case (1, 0, 0, 0):
+                    return CompassDirections.NorthOnly;
+                case (0, 1, 0, 0):
+                    return CompassDirections.SouthOnly;
+                case (0, 0, 1, 0):
+                    return CompassDirections.EastOnly;
+                case (0, 0, 0, 1):
+                    return CompassDirections.WestOnly;
+                default:
+                    // Default solution, also used for roads not yet implemented.
+                    return CompassDirections.NoRoads;
+            }
+        }
+
+        private int GetBinaryForRoads(int combinedcoordinates)
+        {
+           float noiseresult = noise.GetNoise(combinedcoordinates, combinedcoordinates);
+           return (int) (noiseresult % 2);
+        }
+
+
+        private ITile GetTileFromNoise(float noiseResult, int x, int y)
+        {
+            return (noiseResult * 10) switch
             {
                 (< -8) => new WaterTile(x, y),
                 (< -4) => new DirtTile(x, y),
