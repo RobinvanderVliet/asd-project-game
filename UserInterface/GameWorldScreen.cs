65868adfe964ed7db42d36a64d63f492de252595
Jordi Stevens <JSA.Stevens@student.han.nl>
2021-05-31 11:56:16 +0200

    Tonen wereld in UI

--- UserInterface/GameWorldScreen.cs
+++ UserInterface/GameWorldScreen.cs
@@ -12,23 +12,64 @@ namespace UserInterface
         private int _yPosition;
         private int _width;
         private int _height;
-
+        private char[,] _map;
         public GameWorldScreen(int x, int y, int width, int height)
         {
             _xPosition = x;
             _yPosition = y;
             _width = width;
             _height = height;
+            _map = new char[13, 13] {
+            {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','☻','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+             {'.','.','~','~','~','~','~','~','~','~','~','~','~'},
+            };
         }
 
         public override void DrawScreen()
         {
             DrawWorldBox();
+            DrawWorld();
         }
 
         private void DrawWorldBox()
         {
             DrawBox(_xPosition, _yPosition, _width, _height);
+        }
+        private void DrawWorld()
+        {
+            int originalCursorX = Console.CursorLeft;
+            int originalCursorY = Console.CursorTop;
+            for (int i = 0; i < _map.GetLength(0); i++)
+            {
+                Console.SetCursorPosition(_xPosition + 1, _yPosition + OFFSET_TOP + i);
+                for (int j = 0; j < _map.GetLength(1); j++)
+                {
+                    Console.Write(_map[i,j]);
+                    if(j < _map.GetLength(1) - 1)
+                    {
+                        Console.Write("  ");
+                    }
+                }
+            }
+            Console.SetCursorPosition(originalCursorX, originalCursorY);
+        }
+        
+
+        public void UpdateWorld(char[,] newMap)
+        {
+            _map = newMap;
+            DrawWorld();
         }
     }
 }
