615594e43802720c8a139374be5853120472ab48
Wern212 <wdj.vanvoorst@student.han.nl>
2021-06-01 10:39:56 +0200

    Spawn gets a chestTile

--- WorldGeneration/ItemSpawner.cs
+++ WorldGeneration/ItemSpawner.cs
@@ -1,9 +1,14 @@
-﻿namespace WorldGeneration
+﻿using Items;
+using WorldGeneration.Models.LootableTiles;
+
+namespace WorldGeneration
 {
     public class ItemSpawner : ISpawner
     {
         public void Spawn()
         {
+            var chestTile = new ChestTile();
+            chestTile.ItemsOnTile.Add(ItemFactory.GetKnife());
             throw new System.NotImplementedException();
         }
     }
