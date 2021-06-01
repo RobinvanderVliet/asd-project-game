c2c97ff52e358e6a4c985753954152375980f632
Mike <mikevh17@gmail.com>
2021-06-01 16:11:48 +0200

    Added unittests
    Added unittests for testing if chesttile is put in random coordinate in chunk and if chest is filled with item

--- WorldGeneration.Tests/ItemSpawnerTest.cs
+++ WorldGeneration.Tests/ItemSpawnerTest.cs
@@ -1,14 +1,16 @@
 ﻿using NUnit.Framework;
 using Moq;
 using System.Diagnostics.CodeAnalysis;
+using Items;
 using WorldGeneration.Models;
 using WorldGeneration.Models.Interfaces;
+using WorldGeneration.Models.LootableTiles;
+using WorldGeneration.Models.TerrainTiles;
 
 namespace WorldGeneration.Tests
 {
     [ExcludeFromCodeCoverage]  
     [TestFixture]
-    
     public class ItemSpawnerTest
     {
         
@@ -27,20 +29,58 @@ namespace WorldGeneration.Tests
         public void Setup()
         {
             //Initialisation of variables
-            sut = new ItemSpawner(_mockedNoiseObject);
-            _chunk = new Chunk();
+            _chunk = new Chunk(0,0,new ITile[4],2);
+            _chunk.X = 0;
+            _chunk.Y = 0;
 
             //Initialisation of mocks
             _mockedNoise = new Mock<IFastNoise>();
 
             // _mockedNoise.Setup();
-            
             _mockedNoiseObject = _mockedNoise.Object;
+            
+            sut = new ItemSpawner(_mockedNoiseObject);
 
         }
-        
-        
-        
-        
+
+        [Test]
+        public void Test_Spawn_CreateChestTileOnRandomCoordinateInChunk()
+        {
+            //Arrange ---------
+            _mockedNoise.Setup(x => x.GetNoise(_chunk.X, _chunk.Y))
+            .Returns((float) 0.5).Verifiable();
+
+            for (int i = 0; i < _chunk.Map.Length; i++)
+            {
+                _chunk.Map[i] = new GrassTile(i, i);
+            }
+            
+            //Act ---------
+            sut.Spawn(_chunk);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<ChestTile>(_chunk.Map[1]);
+        }
+
+        [Test]
+        public void Test_Spawn_CheckIfChestIsFilledWithItem()
+        {
+            //Arrange ---------
+            _mockedNoise.Setup(x => x.GetNoise(_chunk.X, _chunk.Y))
+                .Returns((float) 0.5).Verifiable();
+
+            for (int i = 0; i < _chunk.Map.Length; i++)
+            {
+                _chunk.Map[i] = new GrassTile(i, i);
+            }
+
+            //Act ---------
+            sut.Spawn(_chunk);
+
+            //Assert ---------
+            Assert.IsInstanceOf<Item>(_chunk.Map[1].ItemsOnTile[0]);
+        }
+
+
     }
 }
