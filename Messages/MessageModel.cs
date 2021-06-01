4bbbc84bc20404959e6376dd5b555c8143e8b3be
Wim Beukers <W.Beukers@student.han.nl>
2021-06-01 17:01:58 +0200

    Added messages project

---
+++ Messages/MessageModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Messages
+{
+    public class MessageModel
+    {
+        private List<string> _messages;
+
+        public MessageModel()
+        {
+            _messages = new(); 
+        }
+
+        public void addMessage(string message)
+        {
+            _messages.Add(message);
+        }
+
+        public Queue<string> GetLatestMessages(int amount)
+        {
+            Queue<string> result = new();
+            for (int x = amount; x > 0 ; x-- ){
+                result.Enqueue(_messages.ElementAt(_messages.Count - x));
+            }
+            return result;
+        }
+    }
+}
---
+++ Messages/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Messages
+{
+    public class MessageService : IMessageService
+    {
+        private MessageModel _messages; 
+        //add screenhandler 
+
+        public void AddMessage(string message)
+        {
+            _messages.addMessage(message);
+            //refresh screenHandler
+        }
+
+        // function that calls _messages.GetLatestMessages(20) and sends it to screenHandler
+
+
+    }
+}
