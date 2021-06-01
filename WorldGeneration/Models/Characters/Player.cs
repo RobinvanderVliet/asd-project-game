a0755af6cac7f17a0745bd4535b75248ed00425a
Robin van der Vliet <info@robinvandervliet.com>
2021-06-01 11:17:26 +0200

    Add max variables for health, stamina and radiation level; refactor add functions

--- WorldGeneration/Models/Characters/Player.cs
+++ WorldGeneration/Models/Characters/Player.cs
@@ -12,44 +12,65 @@ namespace WorldGeneration
         public int Team { get; set; }
 
         //random default values for health&stamina for now
-        public const int HEALTHCAP = 100;
-        public const int STAMINACAP = 100;
-        public const int RADIATIONLEVELCAP = 100;
+        public const int HEALTH_MIN = 0;
+        public const int HEALTH_MAX = 100;
+        public const int STAMINA_MIN = 0;
+        public const int STAMINA_MAX = 100;
+        public const int RADIATION_LEVEL_MIN = 0;
+        public const int RADIATION_LEVEL_MAX = 100;
 
         public Player(string name, int xPosition, int yPosition, string symbol, string id) : base(name, xPosition, yPosition, symbol)
         {
             Id = id;
-            Stamina = STAMINACAP;
-            Health = HEALTHCAP;
+            Stamina = STAMINA_MAX;
+            Health = HEALTH_MAX;
             Inventory = new();
-            RadiationLevel = 10;
+            RadiationLevel = RADIATION_LEVEL_MIN;
             Team = 0;
         }
         
         public void AddStamina(int amount)
         {
             Stamina += amount;
-            if (Stamina > STAMINACAP)
+
+            switch (Stamina)
             {
-                Stamina = STAMINACAP;
+                case < STAMINA_MIN:
+                    Stamina = STAMINA_MIN;
+                    break;
+                case > STAMINA_MAX:
+                    Stamina = STAMINA_MAX;
+                    break;
             }
         }
-        
+
         public void AddRadiationLevel(int amount)
         {
             RadiationLevel += amount;
-            if (RadiationLevel > RADIATIONLEVELCAP)
+
+            switch (RadiationLevel)
             {
-                RadiationLevel = RADIATIONLEVELCAP;
+                case < RADIATION_LEVEL_MIN:
+                    RadiationLevel = RADIATION_LEVEL_MIN;
+                    break;
+                case > RADIATION_LEVEL_MAX:
+                    RadiationLevel = RADIATION_LEVEL_MAX;
+                    break;
             }
         }
-        
+
         public void AddHealth(int amount)
         {
             Health += amount;
-            if (Health > HEALTHCAP)
+
+            switch (Health)
             {
-                Health = HEALTHCAP;
+                case < HEALTH_MIN:
+                    Health = HEALTH_MIN;
+                    break;
+                case > HEALTH_MAX:
+                    Health = HEALTH_MAX;
+                    break;
             }
         }
     }
