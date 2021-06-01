716a6edd7717fb5f87501b693422d24181296f8c
Wern212 <wdj.vanvoorst@student.han.nl>
2021-06-01 12:16:42 +0200

    Unit tests for GetTileFromNoise().

--- WorldGeneration.Tests/NoiseMapGeneratorTest.cs
+++ WorldGeneration.Tests/NoiseMapGeneratorTest.cs
@@ -1,5 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
+using Moq;
 using NUnit.Framework;
+using WorldGeneration.Models.HazardousTiles;
+using WorldGeneration.Models.Interfaces;
+using WorldGeneration.Models.TerrainTiles;
 
 namespace WorldGeneration.Tests
 {
@@ -10,23 +14,145 @@ namespace WorldGeneration.Tests
         //Declaration and initialisation of constant variables
  
         //Declaration of variables
+        private NoiseMapGenerator sut;
+        private int _coordinateX;
+        private int _coordinateY;
  
         //Declaration of mocks
+        private Mock<IFastNoise> _mockedNoise;
+        private IFastNoise _mockedNoiseObject;
  
         [SetUp]
         public void Setup()
         {
             //Initialisation of variables
+            sut = new NoiseMapGenerator();
             //Initialisation of mocks
+            _mockedNoise = new Mock<IFastNoise>();
+            _mockedNoiseObject = _mockedNoise.Object;
+            
+            sut.SetNoise(_mockedNoiseObject);
+        }
+        
+        
+        [Test]
+        public void Test_Function_GetWaterTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) -0.81;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<WaterTile>(result);
+        }
+        
+        [Test]
+        public void Test_Function_GetDirtTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) -0.41;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<DirtTile>(result);
+        }
+        
+        [Test]
+        public void Test_Function_GetGrassTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) 0.1;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<GrassTile>(result);
+        }
+        
+        [Test]
+        public void Test_Function_GetSpikeTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) 0.24;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<SpikeTile>(result);
+        }
+        
+        [Test]
+        public void Test_Function_GetStreetTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) 0.44;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<StreetTile>(result);
+        }
+        
+        
+        [Test]
+        public void Test_Function_GetGasTileFromNoise() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) 1;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<GasTile>(result);
+        }
+                
+        [Test]
+        public void Test_Function_GetTileFromNoiseOutlier() 
+        {
+            //Arrange ---------
+            _coordinateX = 2;
+            _coordinateY = 3;
+            float _noise = (float) 0;
+
+            //Act ---------
+            var result = sut.GetTileFromNoise(_noise, _coordinateX, _coordinateY);
+            
+            //Assert ---------
+            Assert.IsInstanceOf<GrassTile>(result);
         }
         
         [Test]
         public void Test_Function_DoesThing() 
         {
             //Arrange ---------
+            
             //Act ---------
+            
             //Assert ---------
             Assert.That(true);
         }
+        
+        
+        
     }
 }
--- WorldGeneration/NoiseMapGenerator.cs
+++ WorldGeneration/NoiseMapGenerator.cs
@@ -7,33 +7,35 @@ namespace WorldGeneration
 {
     public class NoiseMapGenerator : INoiseMapGenerator
     {
+        private IFastNoise _noise;
+
+        public NoiseMapGenerator()
+        {
+            _noise = new FastNoiseLite();
+            _noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
+            _noise.SetFrequency(0.015f);
+            _noise.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
+        }
+
         public int[,] GenerateAreaMap(int size, int seed)
         {
-            var noise = new FastNoiseLite();
-            noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
-            noise.SetSeed(seed);
-            noise.SetFrequency(0.015f);
-            noise.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
+            _noise.SetSeed(seed);
             var noiseData = new int[size, size];
             for (var y = 0; y < size; y++)
             for (var x = 0; x < size; x++)
-                noiseData[x, y] = (int) noise.GetNoise(x, y);
+                noiseData[x, y] = (int) _noise.GetNoise(x, y);
             return noiseData;
         }
 
         public Chunk GenerateChunk(int chunkX, int chunkY, int chunkRowSize, int seed)
         {
-            var noise = new FastNoiseLite();
-            noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
-            noise.SetSeed(seed);
-            noise.SetFrequency(0.03f);
-            noise.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
+            _noise.SetSeed(seed);
             var map = new ITile[chunkRowSize * chunkRowSize];
             for (var y = 0; y < chunkRowSize; y++)
             {
                 for (var x = 0; x < chunkRowSize; x++)
                 {
-                    map[y * chunkRowSize + x] = GetTileFromNoise(noise.GetNoise(x + chunkX * chunkRowSize, y + chunkY * chunkRowSize)
+                    map[y * chunkRowSize + x] = GetTileFromNoise(_noise.GetNoise(x + chunkX * chunkRowSize, y + chunkY * chunkRowSize)
                         , x + chunkRowSize * chunkX
                         , chunkRowSize * chunkY - chunkRowSize + y);
                 }
@@ -41,8 +43,9 @@ namespace WorldGeneration
             return new Chunk(chunkX, chunkY, map, chunkRowSize);
         }
 
-        private ITile GetTileFromNoise(float noise, int x, int y)
+        public ITile GetTileFromNoise(float noise, int x, int y)
         {
+            // this function is public for unit testing purposes only.
             return (noise * 10) switch
             {
                 (< -8) => new WaterTile(x, y),
@@ -52,6 +55,12 @@ namespace WorldGeneration
                 (< 8) => new StreetTile(x, y),
                 _ => new GasTile(x, y)
             };
+        }
+
+        public void SetNoise(IFastNoise noise)
+        {
+            // This function exists for unit testing purposes.
+            _noise = noise;
         }
     }
 }
