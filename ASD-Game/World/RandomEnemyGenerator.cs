8a86ae42d0d8168dbdabee90e844e750195d4b4c
Werner Voorst <WDJ.vanVoorst@student.han.nl>
2021-06-04 11:30:01 +0200

    Added RandomEnemyGenerator.cs

---
+++ ASD-Game/World/RandomEnemyGenerator.cs
@@ -0,0 +1,25 @@
+﻿using ASD_project.World.Models;
+using Creature.Creature;
+using Items;
+
+namespace ASD_project.World
+{
+    public static class RandomEnemyGenerator
+    {
+
+        public static Monster GetRandomEnemy(float noise, int coordinateX, int coordinateY)
+        {
+            switch (noise %2)
+            {
+                case 0:
+                    return new Monster("Zombie", coordinateX, coordinateY, CharacterSymbol.TERMINATOR, "monst");
+                case 1:
+                    // return new new SmartMonster("Zombie", coordinateX, coordinateY, CharacterSymbol.TERMINATOR, "monst", new DataGatheringService(_worldService));
+                    // Let op! Dit zorgt mogelijk voor een circular dependency!
+                    return null;
+                default:
+                    return null;
+            }
+        }
+    }
+}
