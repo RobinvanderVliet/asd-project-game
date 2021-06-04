9c71426a47d7fed01e3c43739df24d4167587187
R-Gerrits <48597297+R-Gerrits@users.noreply.github.com>
2021-06-04 15:51:50 +0200

    Started setup for AgentTest

--- ASD-Game.Tests/CreatureTests/Creature/AgentTest.cs
+++ ASD-Game.Tests/CreatureTests/Creature/AgentTest.cs
@@ -1,7 +1,33 @@
-﻿namespace Creature.Tests
+﻿using Creature.Creature.StateMachine;
+using Moq;
+using NUnit.Framework;
+
+namespace Creature.Tests
 {
+    [TestFixture]
     public class AgentTest
     {
+        private Agent _sut;
+        private Mock<ICreatureStateMachine> _creatureStateMachineMock;
+
+        private string Id;
+        private string Name;
+        private string Symbol;
+        private int XPosition;
+        private int YPosition;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _creatureStateMachineMock = new Mock<ICreatureStateMachine>();
+            Id = "01";
+            Name = "AgentTest";
+            Symbol = "S";
+            XPosition = 10;
+            YPosition = 10;
+            
+            _sut = new Agent(Name, XPosition, YPosition, Symbol, Id);
+        }
         
     }
 }
