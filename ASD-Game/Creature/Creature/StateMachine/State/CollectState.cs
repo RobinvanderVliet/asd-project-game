a3cc1abc33a616b775ed33324994cd10bf8e5ce2
R-Gerrits <48597297+R-Gerrits@users.noreply.github.com>
2021-06-04 11:52:44 +0200

    Added to BuilderinfoList and BuilderConfiguration to CollectState

--- ASD-Game/Creature/Creature/StateMachine/State/CollectState.cs
+++ ASD-Game/Creature/Creature/StateMachine/State/CollectState.cs
@@ -1,5 +1,6 @@
 ﻿using Creature.Creature.StateMachine.Data;
 using System;
+using System.Collections.Generic;
 using ASD_project.Creature.Creature.StateMachine.Builder;
 using Creature.Creature.StateMachine.Builder;
 
@@ -7,12 +8,12 @@ namespace Creature.Creature.StateMachine.State
 {
     public class CollectState : CreatureState
     {
-        public CollectState(ICreatureData creatureData, ICreatureStateMachine stateMachine, BuilderInfo builderInfo, BuilderConfiguration builderConfiguration) : base(creatureData, stateMachine, builderInfo, builderConfiguration)
+        public CollectState(ICreatureData creatureData, ICreatureStateMachine stateMachine, List<BuilderInfo> builderInfoList, BuilderConfiguration builderConfiguration) : base(creatureData, stateMachine, builderInfoList, builderConfiguration)
         {
             _creatureData = creatureData;
             _stateMachine = stateMachine;
             _builderConfiguration = builderConfiguration;
-            _builderInfo = builderInfo;
+            _builderInfoList = builderInfoList;
         }
 
         public override void Do()
