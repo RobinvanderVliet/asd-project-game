a97afc8568d5147d2229e0d6505ab715ad3784ae
Mike <mikevh17@gmail.com>
2021-06-07 09:17:39 +0200

    Added PR feedback
    Added PR feedback

--- ASD-Game/Agent/FileHandler.cs
+++ ASD-Game/Agent/FileHandler.cs
@@ -8,7 +8,8 @@ namespace ASD_project.Agent
     public class FileHandler
     {
         private string[] _allowedTypes = new[] { ".txt", ".cfg" };
-
+        private static readonly char _separator = Path.DirectorySeparatorChar;
+        
         public virtual string ImportFile(string filepath)
         {
             if (!File.Exists(filepath))
@@ -36,7 +37,7 @@ namespace ASD_project.Agent
 
         public virtual void ExportFile(string content, string fileName)
         {
-            string safeFileLocation = GetBaseDirectory() + "/Resource/" + fileName;
+            string safeFileLocation = GetBaseDirectory() + $"{_separator}Resource{_separator}{fileName}";
 
             CreateDirectory(safeFileLocation);
 
