b09f3b3c00100fb3121777edc0fff63cd3f26bb0
Ugur Ekim <ugurekim@gmail.com>
2021-06-07 15:12:26 +0200

    replaced excludefromcodecoverage to entire class

--- ASD-Game/DatabaseHandler/DbConnection.cs
+++ ASD-Game/DatabaseHandler/DbConnection.cs
@@ -5,11 +5,11 @@ using LiteDB.Async;
 
 namespace ASD_Game.DatabaseHandler
 {
+    [ExcludeFromCodeCoverage]
     public class DBConnection : IDBConnection
     {
         private static readonly char _separator = Path.DirectorySeparatorChar;
         
-        [ExcludeFromCodeCoverage]
         public ILiteDatabaseAsync GetConnectionAsync()
         {
             try
