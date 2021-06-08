d3bcaf4923a3f799a1c0bf5ea58ef6b1d95903a1
Wern212 <wdj.vanvoorst@student.han.nl>
2021-06-08 13:14:45 +0200

    Added improved player spawn and PlayerSpawnTests.cs

---
+++ ASD-Game.Tests/SessionTests/GameConfiguration/PlayerSpawnTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using ASD_Game.ActionHandling;
+using ASD_Game.Agent.Antlr.Ast.Comparables;
+using ASD_Game.DatabaseHandler.POCO;
+using ASD_Game.DatabaseHandler.Services;
+using ASD_Game.Messages;
+using ASD_Game.Network;
+using ASD_Game.Session;
+using ASD_Game.Session.GameConfiguration;
+using ASD_Game.UserInterface;
+using ASD_Game.World.Models;
+using ASD_Game.World.Models.Characters;
+using ASD_Game.World.Models.Interfaces;
+using ASD_Game.World.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace ASD_Game.Tests.SessionTests.GameConfiguration
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class PlayerSpawnTests
+    {
+        //Declaration and initialisation of constant variables
+        private GameSessionHandler _sut;
+        //Declaration of mocks
+        private Mock<IClientController> _mockedClientController;
+        private Mock<ISessionHandler> _mockedSessionHandler;
+        private Mock<IWorldService> _mockedWorldService;
+        private Mock<IRelativeStatHandler> _mockedRelativeStatHandler;
+        private Mock<IMessageService> _mockedMessageService;
+        private Mock<IGameConfigurationHandler> _mockedGameConfigurationHandler;
+        private Mock<IScreenHandler> _mockedScreenHandler;
+        private Mock<IDatabaseService<PlayerPOCO>> _mockedPlayerDatabaseService;
+        private Mock<IDatabaseService<GamePOCO>> _mockedGameDatabaseService;
+        private Mock<IDatabaseService<GameConfigurationPOCO>> _mockedGameConfigDatabaseService;
+        private Mock<IDatabaseService<PlayerItemPOCO>> _mockedPlayerItemDatabaseService;
+        private Mock<ITerrainTile> _mockedTile;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _mockedClientController = new Mock<IClientController>();
+            _mockedSessionHandler = new Mock<ISessionHandler>();
+            _mockedWorldService = new Mock<IWorldService>();
+            _mockedRelativeStatHandler = new Mock<IRelativeStatHandler>();
+            _mockedMessageService = new Mock<IMessageService>();
+            _mockedGameConfigurationHandler = new Mock<IGameConfigurationHandler>();
+            _mockedScreenHandler = new Mock<IScreenHandler>();
+            _mockedPlayerDatabaseService = new Mock<IDatabaseService<PlayerPOCO>>();
+            _mockedGameDatabaseService = new Mock<IDatabaseService<GamePOCO>>();
+            _mockedGameConfigDatabaseService = new Mock<IDatabaseService<GameConfigurationPOCO>>();
+            _mockedPlayerItemDatabaseService = new Mock<IDatabaseService<PlayerItemPOCO>>();
+            _mockedTile = new Mock<ITerrainTile>();
+            
+            int sessionSeed = 12;
+            List<String[]> allClients = new List<string[]>();
+            allClients.Add(new string[]{"1", "Player 1"});
+            allClients.Add(new string[]{"2", "Player 2"});
+            _mockedSessionHandler.Setup(mock => mock.GetSessionSeed()).Returns(sessionSeed);
+            _mockedSessionHandler.Setup(mock => mock.GetAllClients()).Returns(allClients);
+            _mockedWorldService.Setup(mock => mock.LoadArea(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>()));
+            _mockedWorldService.Setup(mock => mock.GetTile(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(_mockedTile.Object);
+            _mockedTile.Setup(mock => mock.IsAccessible).Returns(true);
+            _mockedWorldService.Setup(mock => mock.CheckIfCharacterOnTile(It.IsAny<ITile>())).Returns(false);
+            _mockedClientController.Setup(mock => mock.GetOriginId()).Returns("1");
+
+            _mockedWorldService.Setup(mock => mock.AddPlayerToWorld(It.IsAny<Player>(), true)).Verifiable();
+            _mockedWorldService.Setup(mock => mock.AddPlayerToWorld(It.IsAny<Player>(), false)).Verifiable();
+
+            _sut = new GameSessionHandler(_mockedClientController.Object, _mockedWorldService.Object, _mockedSessionHandler.Object, _mockedRelativeStatHandler.Object, _mockedPlayerDatabaseService.Object,
+                _mockedGameDatabaseService.Object, _mockedGameConfigDatabaseService.Object,  _mockedGameConfigurationHandler.Object, _mockedScreenHandler.Object, _mockedPlayerItemDatabaseService.Object, _mockedMessageService.Object);
+
+        }
+
+        [Test]
+        public void AddPlayersToWorldTest()
+        {
+            // Arrange
+            Player player1 = new Player("Player 1", 13, 15, CharacterSymbol.CURRENT_PLAYER, "1");
+            Player player2 = new Player("Player 2", 13, 16, CharacterSymbol.ENEMY_PLAYER, "2");
+
+            // Act
+            _sut.AddPlayersToWorld();
+
+            // Assert
+            _mockedWorldService.Verify();
+            // _mockedWorldService.Verify(mock => mock.AddPlayerToWorld(player1, true));
+            // _mockedWorldService.Verify(mock => mock.AddPlayerToWorld(player2, false));
+        }
+    }
+}
--- ASD-Game/World/RandomEnemyGenerator.cs
+++ ASD-Game/World/RandomEnemyGenerator.cs
@@ -1,6 +1,4 @@
-﻿using ASD_project.World.Models;
-using Creature.Creature;
-using Items;
+﻿using ASD_Game.Creature.Creature;
 
 namespace ASD_project.World
 {
@@ -12,7 +10,8 @@ namespace ASD_project.World
             switch (noise %2)
             {
                 case 0:
-                    return new Monster("Zombie", coordinateX, coordinateY, CharacterSymbol.TERMINATOR, "monst");
+                    return null;
+                    // return new Monster("Zombie", coordinateX, coordinateY, CharacterSymbol.TERMINATOR, "monst");
                 case 1:
                     // return new new SmartMonster("Zombie", coordinateX, coordinateY, CharacterSymbol.TERMINATOR, "monst", new DataGatheringService(_worldService));
                     // Let op! Dit zorgt mogelijk voor een circular dependency!
