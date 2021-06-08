d50068b55be763080404b6bdcdae7b44c1539e5c
Wim Beukers <wimbeukers96@hotmail.nl>
2021-06-08 17:18:42 +0200

    Added sessionId to packet

--- ASD-Game/Session/SessionHandler.cs
+++ ASD-Game/Session/SessionHandler.cs
@@ -243,7 +243,7 @@ namespace Session
             if (packet.HandlerResponse.ResultMessage != null)
             {
                 StartGameDTO startGameDto = JsonConvert.DeserializeObject<StartGameDTO>(packet.HandlerResponse.ResultMessage);
-                PacketDTO newPacket = new PacketBuilder().SetTarget("client")
+                PacketDTO newPacket = new PacketBuilder().SetTarget("client").SetSessionID(_clientController.SessionId)
                     .SetPayload(packet.HandlerResponse.ResultMessage).SetPacketType(PacketType.GameSession).Build();
                 ((IPacketHandler) _clientController).HandlePacket(newPacket);
             }
