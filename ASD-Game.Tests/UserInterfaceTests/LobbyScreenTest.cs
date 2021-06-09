cfc6ece80f21a82f4784f158d2b0a908b6c89d32
Danny Hageman <DHM.Hageman@student.han.nl>
2021-06-09 10:25:20 +0200

    LobbyScreenTest excluded from code coverage

--- ASD-Game.Tests/UserInterfaceTests/LobbyScreenTest.cs
+++ ASD-Game.Tests/UserInterfaceTests/LobbyScreenTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using NUnit.Framework;
 
 namespace UserInterface.Tests
 {
+    [ExcludeFromCodeCoverage]
     public class LobbyScreenTest
     {
         private LobbyScreen _sut;
