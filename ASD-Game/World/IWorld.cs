712717087dd527fabe0d31227c710407f8e2e125
JillDinnissen <JEH.dinnissen@student.han.nl>
2021-06-09 12:01:47 +0200

    Refacter in Iworld

--- ASD-Game/World/IWorld.cs
+++ ASD-Game/World/IWorld.cs
@@ -7,19 +7,19 @@ namespace ASD_Game.World
     public interface IWorld
     {
         public List<Player> Players { get; set; }
-        public List<Monster> _creatures { get; set; }
+        public List<Monster> Creatures { get; set; }
 
         void UpdateCharacterPosition(string id, int newXPosition, int newYPosition);
 
         void AddPlayerToWorld(Player player, bool isCurrentPlayer = false);
 
-        void AddCreatureToWorld(Monster player);
+        void AddCreatureToWorld(Monster monster);
 
         void UpdateMap();
 
         char[,] GetMapAroundCharacter(Character character);
 
         void DeleteMap();
-        void AddItemToWorld(ItemSpawnDTO itemSpawnDto);
+        void AddItemToWorld(ItemSpawnDTO itemSpawnDTO);
     }
 }
