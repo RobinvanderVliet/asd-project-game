a056524576e0623eebffbb9f703dd49d3eb65997
Bram Verdouw <BC.Verdouw@student.han.nl>
2021-06-09 17:30:19 +0200

    Fixed agent attempting to move when out of stamina

--- ASD-Game/World/Models/Characters/StateMachine/State/WanderState.cs
+++ ASD-Game/World/Models/Characters/StateMachine/State/WanderState.cs
@@ -7,56 +7,58 @@ namespace Creature.Creature.StateMachine.State
 {
     public class WanderState : CharacterState
     {
+        private const int MAX_MOVEMENT_SPEED = 3;
+
         public WanderState(ICharacterData characterData) : base(characterData)
         {
         }
 
         public override void Do()
         {
-            var _builderInfoList = _characterData.BuilderConfigurator.GetBuilderInfoList();
-            var _builderConfiguration = _characterData.BuilderConfigurator;
+            // var _builderInfoList = _characterData.BuilderConfigurator.GetBuilderInfoList();
+            // var _builderConfiguration = _characterData.BuilderConfigurator;
 
-            var steps = new Random().Next(10);
-            //_characterData.MoveHandler.SendMove(PickRandomDirection(), steps);
-            
+            if (_characterData.WorldService.GetPlayer(_characterData.CharacterId).Stamina >= 20)
+            {
+                MoveRandomDirection();
+            }
+
+            // foreach (var builderInfo in _builderInfoList)
+            // {
+            //     if (builderInfo.Action == "wander")
+            //     {
+            //         if (_builderConfiguration.GetGuard(_characterData, _target, builderInfo))
+            //         {
+            //             //int steps = new Random().Next(10);
+            //             //_characterData.MoveHandler.SendMove(PickRandomDirection(), steps);
+            //         }
+            //     }
+            // }
+        }
+
+        private void MoveRandomDirection()
+        {
+            var x = 0;
+            var y = 0;
+
+            if (new Random().Next(10) >= 5)
+            {
+                x += new Random().Next(MAX_MOVEMENT_SPEED);
+            }
+            else
+            {
+                y += new Random().Next(MAX_MOVEMENT_SPEED);
+            }
+
             _characterData.Position = new Vector2(
-                _characterData.Position.X + 2,
-                _characterData.Position.Y
+                _characterData.Position.X + x,
+                _characterData.Position.Y + y
             );
 
             _characterData.MoveHandler.SendAIMove(_characterData.CharacterId,
                 Convert.ToInt32(_characterData.Position.X),
                 Convert.ToInt32(_characterData.Position.Y)
             );
-
-            foreach (var builderInfo in _builderInfoList)
-            {
-                if (builderInfo.Action == "wander")
-                {
-                    if (_builderConfiguration.GetGuard(_characterData, _target, builderInfo))
-                    {
-                        //int steps = new Random().Next(10);
-                        //_characterData.MoveHandler.SendMove(PickRandomDirection(), steps);
-                    }
-                }
-            }
-        }
-
-        private string PickRandomDirection()
-        {
-            var _direction = "";
-            var CaseSwitch = new Random().Next(1, 2);
-            switch (CaseSwitch)
-            {
-                case 1:
-                    _direction += "x";
-                    break;
-                case 2:
-                    _direction += "y";
-                    break;
-            }
-
-            return _direction;
         }
     }
 }
