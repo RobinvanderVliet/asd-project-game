8c50e43d8e3be1e4a31dbe4b8da626f9bd8ee407
Hugo van der Wel <hab.vanderWel@student.han.nl>
2021-06-10 09:14:01 +0200

    made comments nicer

--- ASD-Game/World/Map.cs
+++ ASD-Game/World/Map.cs
@@ -9,7 +9,8 @@ using ASD_Game.World.Models.Interfaces;
 namespace ASD_Game.World
 {
     public class Map : IMap
-    { // This class is documented in chapter "Design Sub-System World Generation" of the SDD.
+    { 
+        // This class is documented in chapter "Design Sub-System World Generation" of the SDD.
         private readonly int _chunkSize;
         private IList<Chunk> _chunks;
         private readonly INoiseMapGenerator _noiseMapGenerator;
@@ -30,7 +31,8 @@ namespace ASD_Game.World
         }
 
         public void LoadArea(int playerX, int playerY, int viewDistance) 
-        { // Gets a list of chunks it has to load. Then generates the ones it can't find in the list of loaded chunks yet.
+        { 
+            // Gets a list of chunks it has to load. Then generates the ones it can't find in the list of loaded chunks yet.
             var chunksWithinLoadingRange = GetListOfChunksWithinLoadingRange(playerX, playerY, viewDistance);
             foreach (var chunkCoordinates in chunksWithinLoadingRange)
             {
@@ -60,9 +62,10 @@ namespace ASD_Game.World
         }
 
         public char[,] GetCharArrayMapAroundCharacter(Character centerCharacter, int viewDistance, List<Character> allCharacters)
-        { // Returns a 2d char array centered around a character.
-          // The view distance is how far the map is rendered to all sides from the character.
-          // The character list should contain all characters you may wish to display.
+        { 
+            // Returns a 2d char array centered around a character.
+            // The view distance is how far the map is rendered to all sides from the character.
+            // The character list should contain all characters you may wish to display.
             if (viewDistance < 0)
             {
                 throw new InvalidOperationException("viewDistance smaller than 0.");
@@ -85,8 +88,9 @@ namespace ASD_Game.World
         }
         
         private string GetDisplaySymbolForSpecificTile(ITile tile, List<Character> characters)
-        { // Returns a string with whichever symbol it can find first in this order:
-          // 1. Character symbol, 2 Item symbol (shows a chest tile), 3 Tile symbol.
+        { 
+            // Returns a string with whichever symbol it can find first in this order:
+            // 1. Character symbol, 2 Item symbol (shows a chest tile), 3 Tile symbol.
             var characterOnTile = characters.FirstOrDefault(character => character.XPosition == tile.XPosition && character.YPosition == tile.YPosition);
             if(characterOnTile != null)
             {
@@ -100,15 +104,17 @@ namespace ASD_Game.World
         }
 
         private Chunk GenerateNewChunk(int chunkX, int chunkY)
-        { // Calls upon the noise map generator to generate a chunk based on a seed. This will ensure the chunk is the same for a given seed every time you generate it.
+        { 
+            // Calls upon the noise map generator to generate a chunk based on a seed. This will ensure the chunk is the same for a given seed every time you generate it.
             return _noiseMapGenerator.GenerateChunk(chunkX, chunkY, _chunkSize);
         }
 
         private Chunk GetLoadedChunkForTileXAndY(int x, int y)
-        { // Tries to find a chunk in the already generated chunks. If it cannot be found it returns null.
-          // It works by converting each chunk's chunk coordinates to standard coordinates. This is done by multiplying the chunk coordinates by the size of the chunk.
-          // Then it checks if the x and y of the coordinates you're looking for fall within the chunk.
-          return _chunks.FirstOrDefault(chunk =>
+        { 
+            // Tries to find a chunk in the already generated chunks. If it cannot be found it returns null.
+            // It works by converting each chunk's chunk coordinates to standard coordinates. This is done by multiplying the chunk coordinates by the size of the chunk.
+            // Then it checks if the x and y of the coordinates you're looking for fall within the chunk.
+            return _chunks.FirstOrDefault(chunk =>
                 chunk.X * _chunkSize <= x 
                 && chunk.X * _chunkSize > x - _chunkSize 
                 && chunk.Y * _chunkSize >= y &&
